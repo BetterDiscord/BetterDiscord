53d8f49fbaae507f415c4e4d6399ff1516521f08
Jiiks <jiiks@windowslive.com>
2015-08-25 17:39:35 +0300

    Proper css

--- betterdiscordapp/index.js
+++ betterdiscordapp/index.js
@@ -2,6 +2,7 @@
  * Version: 1.0
  * Author: Jiiks | http://jiiks.net
  * Date: 25/08/2015 - 08:12
+ * Last Update: 25/08/2015 - 17:38
  * https://github.com/Jiiks/BetterDiscordApp
  */
 
@@ -10,33 +11,29 @@ var _emoteModule = require('./modules/EmoteModule.js');
 var _config = require('./config.json');
 var _helper;
 var _mainWindow;
-var _version = "1.0.0";//https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js
+var _version = "1.0.0";
 
 function BetterDiscordApp(mainWindow) {
     _mainWindow = mainWindow;
     _helper = new _helpers.Helper(mainWindow);
     _helper.getWeb().on('did-finish-load', function() { init(); });
     _helper.getWeb().on('dom-ready', function() {
-        _helper.log("dom-ready");
-        _helper.injectJavaScript("//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js");         //jquery
+
+        _helper.injectJavaScript("//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js");  //jquery
         //TODO make this proper
         setTimeout(function() { //Temporary timer for letting jquery fully load
-            _emoteModule = new _emoteModule.EmoteModule(_helper);
-           // _helper.injectJavaScript("https://rawgit.com/Jiiks/BetterDiscordApp/master/js/dev3.js");        //dev test script
-            //_helper.injectJavaScript("https://cdn.rawgit.com/Jiiks/BetterDiscordApp/master/js/autocomplete.js"); //Autocomplete
+            //TODO LOAD COMMIT HASH, BOTH CSS AND JS WILL HAVE THE SAME COMMIT
+            //Load Main Stylesheet. Note that if you want to load stylesheets, escape them properly.
+            _helper.execJs('$(\'head\').append(\'<link rel=\"stylesheet\" type=\"text\/css\" href=\"https:\/\/rawgit.com\/Jiiks\/BetterDiscordApp\/master\/css\/main.css\">\');');
+            //TODO Load Main minified javascript
+
         }, 5000);
 
     });
 }
 
 function init() {
-
-    _helper.log("Finish loading");
     _helper.log("v" + _version + " initialized.");
-
-    _helper.injectStylesheet("https://raw.githubusercontent.com/Jiiks/BetterDiscordApp/master/css/main.css");
-
-
 }
 
 exports.BetterDiscordApp = BetterDiscordApp;
