87518da7951379127e33e682c3af13ef3ae180d1
Jiiks <jiiks@windowslive.com>
2015-08-25 14:26:36 +0300

    initial temp

---
+++ js/emotemodule.js
@@ -0,0 +1,16 @@
+var observer;
+
+(function() {
+
+    observer = new MutationObserver(function(mutations) {
+        mutations.forEach(mutation) {
+            for(var i = 0 ; i < mutation.addedNodes.length ; ++i) {
+                var next = mutation.addedNodes.item(i);
+                console.log(next);
+            }
+        }
+    });
+
+    obsever.observe(document, {childList: true, subtree: true});
+
+})();
