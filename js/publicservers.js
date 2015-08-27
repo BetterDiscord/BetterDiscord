abf09308c2e33c7f1eabc975a6d3db330a4035a1
Jiiks <jiiks@windowslive.com>
2015-08-27 16:35:07 +0300

    initial base

---
+++ js/publicservers.js
@@ -0,0 +1,14 @@
+/* BetterDiscordApp PublicSevers JavaSctript
+ * Version: 1.0
+ * Author: Jiiks | http://jiiks.net
+ * Date: 27/08/2015 - 14:16
+ * https://github.com/Jiiks/BetterDiscordApp
+ */
+
+function PublicServers() {
+
+}
+
+PublicServers.prototype.init = function() {
+
+}
