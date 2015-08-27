bdea661b02615ae4e654037037cbe94e79f53b58
Jiiks <jiiks@windowslive.com>
2015-08-27 15:40:49 +0300

    app files

---
+++ app/app/index.js
@@ -0,0 +1,12 @@
+/*Note this is partial changes*/
+
+var _betterDiscord = require('betterdiscord');
+
+function launchMainAppWindow(isVisible) {
+
+_betterDiscord = new _betterDiscord.BetterDiscord();
+_betterDiscord.init(mainWindow);
+
+}
+
+ 
