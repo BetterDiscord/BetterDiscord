aeb7836581931b9de08df75aae036600c1f91a2c
Jiiks <jiiks@windowslive.com>
2015-10-24 15:29:37 +0300

    g

--- devjs/main.js
+++ devjs/main.js
@@ -213,9 +213,7 @@ EmoteModule.prototype.injectEmote = function(node) {
 		
         if (emotesTwitch.emotes.hasOwnProperty(word)) {
             parentInnerHTML = parentInnerHTML.replace(word, "<img src=" + twitchEmoteUrlStart + emotesTwitch.emotes[word].image_id + twitchEmoteUrlEnd + " title="+word+"><\/img>");
-		} else if(subEmotesTwitch.emotes.hasOwnProperty(word)) {
-			parentInnerHTML = parentInnerHTML.replace(word, "<img src=" + twitchEmoteUrlStart + subEmotesTwitch.emotes[word].image_id + twitchEmoteUrlEnd + " title="+word+"><\/img>");
-        } else if(typeof emotesFfz !== 'undefined' && settingsCookie["bda-es-1"]) {
+		} else if(typeof emotesFfz !== 'undefined' && settingsCookie["bda-es-1"]) {
             if(emotesFfz.hasOwnProperty(word)) {
                 parentInnerHTML = parentInnerHTML.replace(word, "<img src=" + ffzEmoteUrlStart + emotesFfz[word] + ffzEmoteUrlEnd + " title="+word+"><\/img>");
             } else if(typeof emotesBTTV !== 'undefined' && settingsCookie["bda-es-2"]) {
