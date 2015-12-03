10aa237f294c69c19905a03f07aff7078ff093c6
Jiiks <jiiks@windowslive.com>
2015-12-03 05:53:05 +0200

    k

--- validator.js
+++ validator.js
@@ -31,12 +31,11 @@ exists(bdPath + "/lib/Utils.js");
 
 
 check(discordPath + "/app/app/index.js", "35CAB26E794CDA4123927F2DC932958B");
-
-check(bdPath + "/package.json", "26EF86E0910272D3ACA1B854D2F25340");
-check(bdPath + "/betterdiscord.js", "31DD3E3D37D6FD5F6C2784D06F1162C4");
-check(bdPath + "/lib/BetterDiscord.js", "84D281EDF38F945A5F6DFAAA2D951A6A");
-check(bdPath + "/lib/config.json", "9A6E146B64A2D2D459B081B910D60582");
-check(bdPath + "/lib/Utils.js", "FD1FFCE03E999B80636D200F78383BF8"); 
+check(bdPath + "/package.json", 		 "26EF86E0910272D3ACA1B854D2F25340");
+check(bdPath + "/betterdiscord.js", 	 "31DD3E3D37D6FD5F6C2784D06F1162C4");
+check(bdPath + "/lib/BetterDiscord.js",  "84D281EDF38F945A5F6DFAAA2D951A6A");
+check(bdPath + "/lib/config.json", 		 "9A6E146B64A2D2D459B081B910D60582");
+check(bdPath + "/lib/Utils.js", 		 "FD1FFCE03E999B80636D200F78383BF8"); 
 
 console.log("Everything seems to be fine");
 
