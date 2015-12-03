0f65d7d21dcf76e3531517e4017bc2b65e7a65fc
Jiiks <jiiks@windowslive.com>
2015-12-03 05:05:49 +0200

    Correct utils hash

--- validator.js
+++ validator.js
@@ -36,7 +36,7 @@ check(bdPath + "/package.json", "26EF86E0910272D3ACA1B854D2F25340");
 check(bdPath + "/betterdiscord.js", "31DD3E3D37D6FD5F6C2784D06F1162C4");
 check(bdPath + "/lib/BetterDiscord.js", "84D281EDF38F945A5F6DFAAA2D951A6A");
 check(bdPath + "/lib/config.json", "9A6E146B64A2D2D459B081B910D60582");
-//check(bdPath + "/lib/Utils.js", "8DACE92500A16C8F113863D42001A09D"); weird mismatches
+check(bdPath + "/lib/Utils.js", "FD1FFCE03E999B80636D200F78383BF8"); 
 
 console.log("Everything seems to be fine");
 
