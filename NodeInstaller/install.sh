fce376923acfbc1c03c4040b83c7083b534a444b
Duncan Bristow <duncbristow@msn.com>
2015-12-06 12:32:19 -0700

    Fixed an odd issue where installer prompted to install node, even though it is installed.

--- NodeInstaller/install.sh
+++ NodeInstaller/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # As there is no Linux support, this script assumes OS X as the host system.
 
-command -v node > /dev/null || echo 'Node not found, please download it!' && open 'https://nodejs.org/en/' && sleep 5 && exit
+command -v node > /dev/null || (echo 'Node not found, please download it!' && open 'https://nodejs.org/en/' && sleep 5 && exit)
 
 node index.js 
 exit
