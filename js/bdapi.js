7e9279851391478629844f468723738602b0a8e6
Jiiks <jiiks@windowslive.com>
2015-12-11 06:49:40 +0200

    Initial api

---
+++ js/bdapi.js
@@ -0,0 +1,50 @@
+/* BetterDiscordApp API for Plugins
+ * Version: 1.0
+ * Author: Jiiks | http://jiiks.net
+ * Date: 11/12/2015
+ * Last Update: 11/12/2015
+ * https://github.com/Jiiks/BetterDiscordApp
+ * 
+ * Plugin Template: https://gist.github.com/Jiiks/71edd5af0beafcd08956
+ */
+
+
+//Returns currrent BrowserWindow https://github.com/atom/electron/blob/master/docs/api/browser-window.md
+BdApi.prototype.getBrowserWindow = function() {
+	
+};
+
+//Joins a server
+//code = server invite code
+BdApi.prototype.joinServer = function(code) {
+	
+};
+
+//Inject CSS to document head
+//id = id of element
+//css = custom css
+BdApi.prototype.injectCSS = function(id, css) {
+	
+};
+
+//Clear css/remove any element
+//id = id of element
+BdApi.prototype.clearCSS = function(id) {
+	
+};
+
+//Get another plugin
+//name = name of plugin
+BdApi.prototype.getPlugin = function(name) {
+	
+};
+
+//Get ipc for reason
+BdApi.prototype.getIpc = function() {
+	
+};
+
+//Get BetterDiscord Core
+BdApi.prototype.getCore = function() {
+	
+};
