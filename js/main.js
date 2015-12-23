4fb9cc809b887a5c48633e1ebfd57abfa0f11fba
Jiiks <jiiks@windowslive.com>
2015-12-24 00:01:03 +0200

    addhat

--- js/main.js
+++ js/main.js
@@ -488,10 +488,19 @@ EmoteModule.prototype.injectEmote = function(node) {
 
                 return;
             }
-        
+
             if(word.length < 4) {
                 return;
             }
+			console.log("WORD: " + word);
+			if(word == "ADDHAT") {
+				word = "sirHat";
+				var len = Math.round(word.length / 4);
+                var name =  word.substr(0, len) + "\uFDD9" + word.substr(len, len) + "\uFDD9" + word.substr(len * 2, len) + "\uFDD9" + word.substr(len * 3);
+				var url = "https://cdn.frankerfacez.com/emoticon/51162/1";
+				parentInnerHTML = parentInnerHTML.replace("ADDHAT", '<div class="emotewrapper" style="transform:translate(-35px, -20px);"><img class="emote" alt="' + name + '" src="' + url + '" /><input onclick=\'quickEmoteMenu.favorite(\"'+name+'\", \"'+url+'\");\' class="fav" title="Favorite!" type="button"></div>');
+                return;
+			}
 
             if($.inArray(word, bemotes) != -1) return;
 
