2e9e46ae902f8b67215bfe57d5fae3c7481a5a19
Mitchell <mistermitchell@outlook.com>
2016-01-12 11:43:35 +0000

    Fix for random doubling of public server button

--- js/core.js
+++ js/core.js
@@ -165,7 +165,7 @@ Core.prototype.init = function() {
         console.log(new Date().getTime() + " Defer");
         if($(".guilds-wrapper .guilds").children().length > 0) {
             console.log(new Date().getTime() + " Defer Loaded");
-            var guilds = $(".guilds li:first-child");
+            var guilds = $(".guilds>li:first-child");
 
             guilds.after($("<li></li>", { id: "bd-pub-li", css: { "height": "20px", "display": settingsCookie["bda-gs-1"] == true ? "" : "none" } }).append($("<div/>", { class: "guild-inner", css: { "height": "20px", "border-radius": "4px" } }).append($("<a/>").append($("<div/>", { css: { "line-height": "20px", "font-size": "12px" }, text: "public", id: "bd-pub-button" })))));
 
