76d0b7d9fcf7f9d7caa71b2a5a79875fa6d13f25
Jiiks <jiiks@windowslive.com>
2016-03-31 10:15:39 +0300

    Working autogif plugin

--- Plugins/AutoPlayGifs.plugin.js
+++ Plugins/AutoPlayGifs.plugin.js
@@ -3,27 +3,17 @@
 var agif = function () {};
 
 agif.prototype.convert = function () {
-	$(".attachment-image").each(function() { 
-        var href = $(this).find("canvas").attr("href"); 
+    $(".image canvas").each(function() { 
+        var href = $(this).attr("href"); 
         if(href != undefined) { 
-            $(this).empty(); 
-			$(this).html('<img src="'+href+'"></img>'); 
+            if(href.endsWith(".gif")) {
+                $(this).replaceWith('<img src="'+href+'"></img>'); 
+            }
         } 
     });
-	
-	$(".embed-thumbnail-image").each(function() {
-		var href = $(this).attr("href"); 
-        if(href != undefined) { 
-			if(href.endsWith(".gif")) {
-				$(this).empty(); 
-				$(this).html('<img src="'+href+'"></img>'); 
-			}
-        } 
-	});
 };
 
 agif.prototype.onMessage = function () {
-	console.log("ONMESSAGE");
     this.convert();
 };
 agif.prototype.onSwitch = function () {
@@ -44,7 +34,7 @@ agif.prototype.getName = function () {
     return "Autogif";
 };
 agif.prototype.getDescription = function () {
-    return "Don't use currently broken.";
+    return "Autoplay gifs without having to hover.";
 };
 agif.prototype.getVersion = function () {
     return "0.1.0";
