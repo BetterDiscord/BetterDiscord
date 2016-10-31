fc53df5e4bd5ae27665f60b4834b9818e9b85349
Jiiks <jiiks@windowslive.com>
2016-10-31 17:57:12 +0200

    Default configs

--- v2/lib/core.js
+++ v2/lib/core.js
@@ -15,24 +15,33 @@ const
     _config = require('./config.json'),
     {_utils} = require('./utils'),
     {_logger} = require('./utils'),
-    {EOL} = require('os');
+    {EOL} = require('os'),
+    _os = require('os');
     
 const _startDate = new Date();
 
 var _cfg;
 
-const _defaults = {};
+//Default configs if for some reason we don't receive config params
+const _defaults = {
+    installPath:  (_os.platform() === 'win32' ? process.env.APPDATA : _os.platform() === 'darwin' ? `${process.env.HOME}/Library/Preferences`: `/var/local`) + '/BetterDiscord/lib/',
+    dataPath: (_os.platform() === 'win32' ? process.env.APPDATA : _os.platform() === 'darwin' ? `${process.env.HOME}/Library/Preferences`: `/var/local`) + '/BetterDiscord/data/',
+    emotesEnabled: true
+};
 
 class Core {
     
     constructor(args) {
-
+        this.initConfig(args.cfg);
     }
     
     initConfig(args) {
-        
+        _cfg = _config.cfg;
+        _cfg.installPath = args.installPath || _defaults.installPath;
+        _cfg.dataPath = args.dataPath || _defaults.dataPath;
+        _cfg.emotesEnabled = args.emotesEnabled || _defaults.emotesEnabled;
     }
     
 }
 
-exports.BetterDiscord = new Core(null);
+exports.BetterDiscord = new Core({ cfg: { } });
