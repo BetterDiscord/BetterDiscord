48e55c9417687abedeb708560116d976fe4336c3
Jiiks <jiiks@windowslive.com>
2016-11-04 06:49:51 +0200

    Timestamps and init log

--- v2/src/js/utils.js
+++ v2/src/js/utils.js
@@ -18,15 +18,23 @@ define(() => {
         }
         
         log(msg) {
-            console.log(`%c[%cBetterDiscord|INF%c] %c${msg}`, 'color: #000;', 'color: green; font-weight:#000;', 'color:#00;', '');
+            console.log(`[${this.timeString}]%c[%cBetterDiscord|INF%c] %c${msg}`, 'color: #000;', 'color: green; font-weight:#000;', 'color:#00;', '');
         }
         
         warn(msg) {
-            console.log(`%c[%cBetterDiscord|WRN%c] %c${msg}`, 'color: #000;', 'color: orange; font-weight:700;', 'color:#000;', '');
+            console.log(`[${this.timeString}]%c[%cBetterDiscord|WRN%c] %c${msg}`, 'color: #000;', 'color: orange; font-weight:700;', 'color:#000;', '');
         }
         
         err(msg) {
-            console.log(`%c[%cBetterDiscord|ERR%c] %c${msg}`, 'color: #000;', 'color: red; font-weight:700;', 'color:#000;', '');
+            console.log(`[${this.timeString}]%c[%cBetterDiscord|ERR%c] %c${msg}`, 'color: #000;', 'color: red; font-weight:700;', 'color:#000;', '');
+        }
+        
+        get dateString() {
+            return new Date().toLocaleString("en-GB");
+        }
+        
+        get timeString() {
+            return new Date().toTimeString().split(' ')[0];
         }
         
     }
