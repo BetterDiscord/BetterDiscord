7c4b58abd9cffcc9b916042b1f5b8270cca5040b
Zack Rauen <rauenzi@outlook.com>
2019-05-29 01:00:33 -0400

    add minified version

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
 const path = require("path");
-const webpack = require("webpack");
 
-module.exports = env => {
-  return {
-    mode: "development",
-    target: "node",
-    devtool: "none",
-    entry: "./src/index.js",
-    output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "js"),
-      library: "Core",
-      libraryTarget: "var",
-      libraryExport: "default"
-    },
-    externals: {
-      electron: `require("electron")`,
-      fs: `require("fs")`,
-      path: `require("path")`,
-      request: `require("request")`,
-      events: `require("events")`
-    },
-    resolve: {
-      extensions: [".js"],
-      modules: [
-        path.resolve("src", "data"),
-        path.resolve("src", "modules"),
-        path.resolve("src", "ui")
-      ]
-    }
-  };
+module.exports = {
+  mode: "development",
+  target: "node",
+  devtool: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "js"),
+    library: "Core",
+    libraryTarget: "var",
+    libraryExport: "default"
+  },
+  externals: {
+    electron: `require("electron")`,
+    fs: `require("fs")`,
+    path: `require("path")`,
+    request: `require("request")`,
+    events: `require("events")`
+  },
+  resolve: {
+    extensions: [".js"],
+    modules: [
+      path.resolve("src", "data"),
+      path.resolve("src", "modules"),
+      path.resolve("src", "ui")
+    ]
+  }
 };
