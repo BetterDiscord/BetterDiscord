14243ec0250b7ef4e29e73cf57757aa4193bc10a
Strencher <46447572+Strencher@users.noreply.github.com>
2020-07-23 20:47:26 +0200

    maybe fix githubs autoformatting

--- src/builtins/emotes/emotemenu.js
+++ src/builtins/emotes/emotemenu.js
@@ -17,25 +17,27 @@ export default new class EmoteMenu extends Builtin {
         if (this.hideEmojis) this.enableHideEmojis();
         this.before(EmojiPicker, "type", (_, [args], ret) => {
             if(args.expressionPickerView == "emoji" && this.hideEmojis) args.expressionPickerView = "gif";
-        }) 
+        }); 
 		this.after(EmojiPicker, "type", (_, [args], ret) => {
 			const head = Utilities.getNestedProp(ret, "props.children.props.children.1.props.children.0.props.children.props.children");
 			const body = Utilities.getNestedProp(ret, "props.children.props.children.1.props.children");
 			if(!head || !body) return ret;
 			const selected = args.expressionPickerView;
 			let tabProps = head[0].props;
-			head.push(...Tabs.map(e => React.createElement("div", {
-				id: e.id+"-tab",
-				role: "tab",
-				"aria-selected": selected == e.id,
-				className: tabProps.className,
-				children: React.createElement(tabProps.children.type, {
-					viewType: e.id,
-					isActive: selected == e.id,
-					children: e.label,
-					setActiveView: tabProps.children.props.setActiveView
-				})
-            })))
+			head.push(
+                ...Tabs.map(e => React.createElement("div", {
+                    id: e.id+"-tab",
+                    role: "tab",
+                    "aria-selected": selected == e.id,
+                    className: tabProps.className,
+                    children: React.createElement(tabProps.children.type, {
+                        viewType: e.id,
+                        isActive: selected == e.id,
+                        children: e.label,
+                        setActiveView: tabProps.children.props.setActiveView
+                    })
+                })
+            ));
             if(Tabs.map(e => e.id).includes(selected)) body[2] = Tabs.find(e=>e.id == selected).element();
             if(this.hideEmojis) head.splice(head.findIndex(e=>e.props.id == "emoji-picker-tab"), 1);
 		})
