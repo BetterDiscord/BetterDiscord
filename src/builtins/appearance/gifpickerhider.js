f2f6460dbd52e13dbed016b8fdfb2257b689f6dc
Strencher <46447572+Strencher@users.noreply.github.com>
2020-10-24 21:44:54 +0200

    fix linter issues

--- src/builtins/appearance/gifpickerhider.js
+++ src/builtins/appearance/gifpickerhider.js
@@ -10,16 +10,18 @@ export default new class GifPickerHider extends Builtin {
     enabled() {
         this.unpatch = this.after(WebpackModules.find(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer").type, "render", (_, __, returnValue) => {
             const buttons = Utilities.getNestedProp(returnValue, "props.children.props.children.props.children.1.props.children.props.children.2.props.children");
-            if (Array.isArray(buttons)) for(const button of buttons) {
-                if(!button) continue;
-                const renderFunc = Utilities.getNestedProp(button, "type.type.render");
-                if(!renderFunc) continue;
-
-                // "ChannelGIFPickerButton" for sticker button
-                if(["ChannelStickerPickerButton"].includes(renderFunc.displayName)) {
-                    button.props.disabled = true;
-                    break;
-                };
+            if (Array.isArray(buttons)) {
+                for (const button of buttons) {
+                    if (!button) continue;
+                    const renderFunc = Utilities.getNestedProp(button, "type.type.render");
+                    if (!renderFunc) continue;
+    
+                    // "ChannelGIFPickerButton" for sticker button
+                    if (["ChannelStickerPickerButton"].includes(renderFunc.displayName)) {
+                        button.props.disabled = true;
+                        break;
+                    }
+                }
             }
         });
     }
--- src/builtins/appearance/nitrogiftpickerhider.js
+++ src/builtins/appearance/nitrogiftpickerhider.js
@@ -10,13 +10,15 @@ export default new class NitroGiftPickerHider extends Builtin {
     enabled() {
         this.unpatch = this.after(WebpackModules.find(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer").type, "render", (_, __, returnValue) => {
             const buttons = Utilities.getNestedProp(returnValue, "props.children.props.children.props.children.1.props.children.props.children.2.props.children");
-            if (Array.isArray(buttons)) for(const button of buttons) {
-                if(!button || !button.props) continue;
-                if (button.props.hasOwnProperty("shouldRenderPremiumGiftButton")) {
-                    button.props.shouldRenderPremiumGiftButton = false;
-                    button.props.disabled = true;
-                    break;
-                };
+            if (Array.isArray(buttons)) {
+                for (const button of buttons) {
+                    if (!button || !button.props) continue;
+                    if (button.props.hasOwnProperty("shouldRenderPremiumGiftButton")) {
+                        button.props.shouldRenderPremiumGiftButton = false;
+                        button.props.disabled = true;
+                        break;
+                    }
+                }
             }
         });
     }
