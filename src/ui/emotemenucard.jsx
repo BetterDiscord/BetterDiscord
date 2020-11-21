74fba577669f3a0d42f4c236d214d6d3bed72f96
Tropical <42101043+Tropix126@users.noreply.github.com>
2020-11-20 23:16:15 -0600

    Remove unnessecary className

--- src/ui/emotemenucard.jsx
+++ src/ui/emotemenucard.jsx
@@ -3,7 +3,7 @@ const {ScrollerAuto: Scroller} = WebpackModules.getByProps("ScrollerAuto");
 export default class EmoteMenuCard extends React.Component {
     render() {
         return <div className={`bd-emote-menu`}>
-            <Scroller className="bd-emote-scroller thin-1ybCId">
+            <Scroller className="bd-emote-scroller">
                 <div className="bd-emote-menu-inner">
                     {this.props.children}
                 </div>
