f83e053565131084ff249e163e35403bb0a21de2
Strencher <46447572+Strencher@users.noreply.github.com>
2021-01-09 04:28:33 +0100

    Fix colored text for stable (#530)
    * Fix colored text for stable
    
    * that didn't happen...
    
    * fix typo

--- src/modules/coloredText.js
+++ src/modules/coloredText.js
@@ -2,6 +2,9 @@ import {settingsCookie} from "../0globals";
 import BDV2 from "./v2";
 import Utils from "./utils";
 
+const ChannelStore = BDV2.WebpackModules.findByUniqueProperties(["getChannel", "getGuildChannels"]);
+const GuildMemberStore = BDV2.WebpackModules.findByUniqueProperties(["getMember"]);
+
 export default new class ColoredText {
     injectColoredText() {
         if (this.cancelColoredText) return;
@@ -15,9 +18,10 @@ export default new class ColoredText {
             if (!messageContent.type || !messageContent.type.type || messageContent.type.type.displayName != "MessageContent") return;
             const originalType = messageContent.type.type;
             if (originalType.__originalMethod) return; // Don't patch again
+            const self = this;
             messageContent.type.type = function(props) {
                 const returnValue = originalType(props);
-                const roleColor = settingsCookie["bda-gs-7"] ? props.message.colorString || "" : "";
+                const roleColor = settingsCookie["bda-gs-7"] ? self.getRoleColor(props.message.channel_id, props.message.author.id) || "" : "";
                 returnValue.props.style = {color: roleColor};
                 return returnValue;
             };
@@ -25,6 +29,14 @@ export default new class ColoredText {
             messageContent.type.type.__originalMethod = originalType;
             Object.assign(messageContent.type.type, originalType);
         }});
+    }
+
+    getRoleColor(channelId, memberId) {
+        const channel = ChannelStore.getChannel(channelId);
+        if (!channel) return "";
+        const member = GuildMemberStore.getMember(channel.guild_id, memberId);
+        if (!member) return "";
+        return member.colorString;
     }
 
     removeColoredText() {
