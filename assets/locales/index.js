e678358cca48ee1c927a0a7f4cb0130832411540
Marmota <jaimeadanielfilho@hotmail.com>
2021-04-11 02:22:38 -0300

    Add brazilian Portuguese translation

--- assets/locales/index.js
+++ assets/locales/index.js
@@ -2,5 +2,6 @@ module.exports = {
     en: require("./en.json"),
     de: require("./de.json"),
     ja: require("./ja.json"),
-    sv: require("./sv.json")
+    sv: require("./sv.json"),
+    'pt-br': require("./pt-br.json")
 };
