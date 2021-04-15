2779d9bc4f5bff18e4046317fcc648047e186272
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-04-15 17:08:00 -0500

    Update various pieces of UI
    - fix minimal mode
    - revamp addon error modal
    - restyle some aspects of toasts (includes light mode support)
    - overhaul addon error modal (thanks to @Strencher for some react help)
    - update switches to match discord's latest design

--- renderer/src/ui/settings/components/switch.jsx
+++ renderer/src/ui/settings/components/switch.jsx
@@ -16,8 +16,17 @@ export default class Switch extends React.Component {
     render() {
         const enabledClass = this.props.disabled ? " bd-switch-disabled" : "";
         const checkedClass = this.state.checked ? " bd-switch-checked" : "";
-        return <div className={`bd-switch` + enabledClass + checkedClass}>
-                    <input type="checkbox" id={this.props.id} className={`bd-checkbox`} disabled={this.props.disabled} checked={this.state.checked} onChange={this.onChange} />
-                </div>;
+        return <div className={`bd-switch` + enabledClass + checkedClass} disabled={this.props.disabled} checked={this.state.checked} onChange={this.onChange}>
+            <input id={this.props.id} type="checkbox" />
+            <div className="bd-switch-body">
+                <svg className="bd-switch-slider" viewBox="0 0 28 20" preserveAspectRatio="xMinYMid meet">
+                <rect className="bd-switch-handle" fill="white" x="4" y="0" height="20" width="20" rx="10"></rect>
+                    <svg className="bd-switch-symbol" viewBox="0 0 20 20" fill="none">
+                        <path></path>
+                        <path></path>
+                    </svg>
+                </svg>
+            </div>
+        </div>;
     }
 }
