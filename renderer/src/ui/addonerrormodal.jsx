4e14e43f4287dc1b2fd0329dbd70c74ab2ff1ec7
Kyza <kyza@kyza.net>
2021-04-17 17:43:06 -0400

    Dirtying

--- renderer/src/ui/addonerrormodal.jsx
+++ renderer/src/ui/addonerrormodal.jsx
@@ -1,14 +1,14 @@
-import { React, Strings, WebpackModules } from "modules";
+import {React, Strings, WebpackModules} from "modules";
 import Extension from "./icons/extension";
 import ThemeIcon from "./icons/theme";
 import DetailsIcon from "./icons/details";
 import ErrorBoundary from "./errorboundary";
 
-const { useState, useEffect } = React;
+const {useState, useEffect} = React;
 
 const ButtonModule = WebpackModules.getByProps("ButtonColors");
 const TabBarModule = WebpackModules.getModule(
-	(m) => m?.default?.displayName === "TabBar"
+    (m) => m?.default?.displayName === "TabBar"
 );
 const ModalModule = WebpackModules.getByProps("ModalRoot");
 
@@ -26,173 +26,172 @@ const Button = ButtonModule?.default;
 const expandIconClasses = WebpackModules.getByProps("expandIcon");
 const dividerClasses = WebpackModules.getByProps("topDivider");
 const detailsClasses = WebpackModules.getByProps(
-	"detailsWrapper",
-	"detailsIcon"
+    "detailsWrapper",
+    "detailsIcon"
 );
 const markupClasses = {
-	...WebpackModules.getByProps("blockquoteContainer", "markup"),
-	...WebpackModules.getByProps("zalgo", "messageContent"),
+    ...WebpackModules.getByProps("blockquoteContainer", "markup"),
+    ...WebpackModules.getByProps("zalgo", "messageContent"),
 };
 
 const parser = Object(WebpackModules.getByProps("defaultRules", "parse"));
 
 export default function AddonErrorModal(props) {
-	const [selectedTab, setSelectedTab] = useState("plugins");
-	const [errors, setErrors] = useState([]);
+    const [selectedTab, setSelectedTab] = useState("plugins");
+    const [errors, setErrors] = useState([]);
 
-	useEffect(() => {
-		switch (selectedTab) {
-			case "plugins":
-				setErrors(
-					props.pluginErrors.map((error, index) => (
-						<AddonError
-							key={`${error.name}-${index}`}
-							type={selectedTab}
-							error={error}
-						/>
-					))
-				);
-				break;
-			case "themes":
-				setErrors(
-					props.themeErrors.map((error, index) => (
-						<AddonError
-							key={`${error.name}-${index}`}
-							type={selectedTab}
-							error={error}
-						/>
-					))
-				);
-				break;
-			default:
-				setErrors(<Text>{Strings.Modals.nevermind}</Text>);
-				break;
-		}
-	}, [JSON.stringify(errors), selectedTab]);
+    useEffect(() => {
+        switch (selectedTab) {
+            case "plugins":
+                setErrors(
+                    props.pluginErrors.map((error, index) => (
+                        <AddonError
+                            key={`${error.name}-${index}`}
+                            type={selectedTab}
+                            error={error}
+                        />
+                    ))
+                );
+                break;
+            case "themes":
+                setErrors(
+                    props.themeErrors.map((error, index) => (
+                        <AddonError
+                            key={`${error.name}-${index}`}
+                            type={selectedTab}
+                            error={error}
+                        />
+                    ))
+                );
+                break;
+            default:
+                setErrors(<Text>{Strings.Modals.nevermind}</Text>);
+                break;
+        }
+    }, [JSON.stringify(errors), selectedTab]);
 
-	return (
-		<ErrorBoundary
-			fallback={() => <Text>{Strings.Addons.errorLoadingErrors}</Text>}
-		>
-			<ModalModule.ModalHeader>
-				<Flex direction={Flex.Direction.VERTICAL}>
-					<Flex justify={Flex.Justify.SPACE_BETWEEN}>
-						<FormTitle tag="h4">{Strings.Modals.addonErrors}</FormTitle>
-						<ModalModule.ModalCloseButton onClick={props.onClose} />
-					</Flex>
-					<TabBar
-						look={TabBarModule.Looks.GREY}
-						type={TabBarModule.Types.TOP_PILL}
-						selectedItem={selectedTab}
-						onItemSelect={(item) => setSelectedTab(item)}
-					>
-						<TabBarModule.Item
-							id="plugins"
-							className="bd-addon-error-tabbar-item"
-						>
-							{Strings.Panels.plugins}
-						</TabBarModule.Item>
-						<TabBarModule.Item
-							id="themes"
-							className="bd-addon-error-tabbar-item"
-						>
-							{Strings.Panels.themes}
-						</TabBarModule.Item>
-					</TabBar>
-				</Flex>
-			</ModalModule.ModalHeader>
-			<ModalModule.ModalContent className="bd-addon-error-content">
-				<ErrorBoundary
-					fallback={() => <Text>{Strings.Addons.errorLoadingErrors}</Text>}
-				>
-					{errors}
-				</ErrorBoundary>
-			</ModalModule.ModalContent>
-			<ModalModule.ModalFooter>
-				<Button color={ButtonModule.ButtonColors.BRAND} onClick={props.onClose}>
-					{Strings.Modals.done}
-				</Button>
-				<Button
-					style={{ marginRight: "8px" }}
-					color={ButtonModule.ButtonColors.TRANSPARENT}
-					onClick={() => {
-						props.onClose();
-						// Discord.
-						UserSettingsModule.open("Settings");
-						setTimeout(() => UserSettingsModule.open("plugins"), 1e1);
-					}}
-				>
-					{Strings.Addons.pluginSettings}
-				</Button>
-				<Button
-					style={{ marginRight: "8px" }}
-					color={ButtonModule.ButtonColors.TRANSPARENT}
-					onClick={() => {
-						props.onClose();
-						// Discord.
-						UserSettingsModule.open("Settings");
-						setTimeout(() => UserSettingsModule.open("themes"), 1e1);
-					}}
-				>
-					{Strings.Addons.themeSettings}
-				</Button>
-			</ModalModule.ModalFooter>
-		</ErrorBoundary>
-	);
+    return (
+        <ErrorBoundary
+            fallback={() => <Text>{Strings.Addons.errorLoadingErrors}</Text>}
+        >
+            <ModalModule.ModalHeader>
+                <Flex direction={Flex.Direction.VERTICAL}>
+                    <Flex justify={Flex.Justify.SPACE_BETWEEN}>
+                        <FormTitle tag="h4">{Strings.Modals.addonErrors}</FormTitle>
+                        <ModalModule.ModalCloseButton onClick={props.onClose} />
+                    </Flex>
+                    <TabBar
+                        look={TabBarModule.Looks.GREY}
+                        type={TabBarModule.Types.TOP_PILL}
+                        selectedItem={selectedTab}
+                        onItemSelect={(item) => setSelectedTab(item)}
+                    >
+                        <TabBarModule.Item
+                            id="plugins"
+                            className="bd-addon-error-tabbar-item"
+                        >
+                            {Strings.Panels.plugins}
+                        </TabBarModule.Item>
+                        <TabBarModule.Item
+                            id="themes"
+                            className="bd-addon-error-tabbar-item"
+                        >
+                            {Strings.Panels.themes}
+                        </TabBarModule.Item>
+                    </TabBar>
+                </Flex>
+            </ModalModule.ModalHeader>
+            <ModalModule.ModalContent className="bd-addon-error-content">
+                <ErrorBoundary
+                    fallback={() => <Text>{Strings.Addons.errorLoadingErrors}</Text>}
+                >
+                    {errors}
+                </ErrorBoundary>
+            </ModalModule.ModalContent>
+            <ModalModule.ModalFooter>
+                <Button color={ButtonModule.ButtonColors.BRAND} onClick={props.onClose}>
+                    {Strings.Modals.done}
+                </Button>
+                <Button
+                    style={{marginRight: "8px"}}
+                    color={ButtonModule.ButtonColors.TRANSPARENT}
+                    onClick={() => {
+                        props.onClose();
+                        // Discord.
+                        UserSettingsModule.open("Settings");
+                        setTimeout(() => UserSettingsModule.open("plugins"), 1e1);
+                    }}
+                >
+                    {Strings.Addons.pluginSettings}
+                </Button>
+                <Button
+                    style={{marginRight: "8px"}}
+                    color={ButtonModule.ButtonColors.TRANSPARENT}
+                    onClick={() => {
+                        props.onClose();
+                        // Discord.
+                        UserSettingsModule.open("Settings");
+                        setTimeout(() => UserSettingsModule.open("themes"), 1e1);
+                    }}
+                >
+                    {Strings.Addons.themeSettings}
+                </Button>
+            </ModalModule.ModalFooter>
+        </ErrorBoundary>
+    );
 }
 
 export function AddonError(props) {
-	const [expanded, setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
-	return (
-		<Flex
-			className={`bd-addon-error ${expanded ? "expanded" : "collapsed"}`}
-			direction={Flex.Direction.VERTICAL}
-		>
-			<Flex
-				className="bd-addon-error-header"
-				align={Flex.Align.CENTER}
-				onClick={() => {
-					setExpanded(!expanded);
-				}}
-			>
-				<div className="bd-addon-error-icon">
-					{props.type === "plugins" ? <Extension /> : <ThemeIcon />}
-				</div>
-				<Flex direction={Flex.Direction.VERTICAL}>
-					<Header tag="h3" size={Header.Sizes.SIZE_16}>
-						{props.error.name}
-					</Header>
-					<Flex
-						align={Flex.Align.CENTER}
-						wrap={Flex.Wrap.WRAP}
-						className={`bd-addon-error-details ${detailsClasses.detailsWrapper}`}
-					>
-						<DetailsIcon />
-						<Text
-							color={Text.Colors.HEADER_SECONDARY}
-							size={Text.Sizes.SIZE_12}
-						>
-							{props.error.message}
-						</Text>
-					</Flex>
-				</Flex>
-				<Caret className={expandIconClasses.expandIcon} expanded={expanded} />
-			</Flex>
-			{expanded ? (
-				<>
-					<FormDivider className={dividerClasses.topDivider} />
-					<div
-						className={`bd-addon-error-stack ${markupClasses.markup} ${markupClasses.messageContent}`}
-					>
-						{parser.defaultRules.codeBlock.react(
-							{ content: props.error.stack, lang: "ruby" },
-							null,
-							{}
-						)}
-					</div>
-				</>
-			) : null}
-		</Flex>
-	);
+    return (
+        <Flex
+            className={`bd-addon-error ${expanded ? "expanded" : "collapsed"}`}
+            direction={Flex.Direction.VERTICAL}
+        >
+            <Flex
+                align={Flex.Align.CENTER}
+                onClick={() => {
+                    setExpanded(!expanded);
+                }}
+            >
+                <div className="bd-addon-error-icon">
+                    {props.type === "plugins" ? <Extension /> : <ThemeIcon />}
+                </div>
+                <Flex direction={Flex.Direction.VERTICAL}>
+                    <Header tag="h3" size={Header.Sizes.SIZE_16}>
+                        {props.error.name}
+                    </Header>
+                    <Flex
+                        align={Flex.Align.CENTER}
+                        wrap={Flex.Wrap.WRAP}
+                        className={`bd-addon-error-details ${detailsClasses.detailsWrapper}`}
+                    >
+                        <DetailsIcon />
+                        <Text
+                            color={Text.Colors.HEADER_SECONDARY}
+                            size={Text.Sizes.SIZE_12}
+                        >
+                            {props.error.message}
+                        </Text>
+                    </Flex>
+                </Flex>
+                <Caret className={expandIconClasses.expandIcon} expanded={expanded} />
+            </Flex>
+            {expanded ? (
+                <>
+                    <FormDivider className={dividerClasses.topDivider} />
+                    <div
+                        className={`bd-addon-error-stack ${markupClasses.markup} ${markupClasses.messageContent}`}
+                    >
+                        {parser.defaultRules.codeBlock.react(
+                            {content: props.error.stack, lang: "ruby"},
+                            null,
+                            {}
+                        )}
+                    </div>
+                </>
+            ) : null}
+        </Flex>
+    );
 }
--- renderer/src/ui/errorboundary.jsx
+++ renderer/src/ui/errorboundary.jsx
@@ -1,74 +1,74 @@
 import Logger from "common/logger";
-import { React, Strings, WebpackModules } from "modules";
+import {React, Strings, WebpackModules} from "modules";
 
 const Text = WebpackModules.getByDisplayName("Text");
 
-let reactErrors;
-fetch(
-	"https://raw.githubusercontent.com/facebook/react/master/scripts/error-codes/codes.json"
-)
-	.then((response) => response.json())
-	.then((body) => {
-		reactErrors = body;
-	});
+// let reactErrors;
+// fetch(
+//     "https://raw.githubusercontent.com/facebook/react/master/scripts/error-codes/codes.json"
+// )
+//     .then((response) => response.json())
+//     .then((body) => {
+//         reactErrors = body;
+//     });
 
-export default class ErrorBoundary extends React.Component {
-	constructor(props) {
-		super(props);
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			hasError: false,
-		};
-	}
+        this.state = {
+            hasError: false,
+        };
+    }
 
-	static getDerivedStateFromError(error) {
-		return { hasError: true, error };
-	}
+    static getDerivedStateFromError(error) {
+        return {hasError: true, error};
+    }
 
-	componentDidCatch(error, info) {
-		this.setState({ hasError: true, error, info }, () => {
-			this.props.onError?.(this.state);
-		});
-	}
+    componentDidCatch(error, info) {
+        this.setState({hasError: true, error, info}, () => {
+            this.props.onError?.(this.state);
+        });
+    }
 
-	render() {
-		if (this.state.hasError) {
-			const Fallback = this.props.fallback ?? false;
-			Logger.log(Fallback);
-			if (Fallback) {
-				return (
-					<ErrorBoundary
-						fallback={() => (
-							<ErrorBoundary
-								fallback={() => (
-									<ErrorBoundary>
-										{Strings.Addons.errorBoundaryErrorError}
-									</ErrorBoundary>
-								)}
-							>
-								<Text>{Strings.Addons.errorBoundaryError}</Text>
-							</ErrorBoundary>
-						)}
-					>
-						<Fallback />
-					</ErrorBoundary>
-				);
-			}
-			return null;
-		}
-		return this.props.children;
-	}
+    render() {
+        if (this.state.hasError) {
+            const Fallback = this.props.fallback ?? false;
+            Logger.log(Fallback);
+            if (Fallback) {
+                return (
+                    <ErrorBoundary
+                        fallback={() => (
+                            <ErrorBoundary
+                                fallback={() => (
+                                    <ErrorBoundary>
+                                        {Strings.Addons.errorBoundaryErrorError}
+                                    </ErrorBoundary>
+                                )}
+                            >
+                                <Text>{Strings.Addons.errorBoundaryError}</Text>
+                            </ErrorBoundary>
+                        )}
+                    >
+                        <Fallback />
+                    </ErrorBoundary>
+                );
+            }
+            return null;
+        }
+        return this.props.children;
+    }
 }
 
-// const originalRender = ErrorBoundary.prototype.render;
-// Object.defineProperty(ErrorBoundary.prototype, "render", {
-// 	enumerable: false,
-// 	configurable: false,
-// 	set: function () {
-// 		Logger.warn(
-// 			"ErrorBoundary",
-// 			"Addon policy for plugins #5 https://github.com/rauenzi/BetterDiscordApp/wiki/Addon-Policies#plugins"
-// 		);
-// 	},
-// 	get: () => originalRender,
-// });
+const originalRender = ErrorBoundary.prototype.render;
+export default Object.defineProperty(ErrorBoundary.prototype, "render", {
+    enumerable: false,
+    configurable: false,
+    set: function () {
+        Logger.warn(
+            "ErrorBoundary",
+            "Addon policy for plugins #5 https://github.com/rauenzi/BetterDiscordApp/wiki/Addon-Policies#plugins"
+        );
+    },
+    get: () => originalRender,
+});
