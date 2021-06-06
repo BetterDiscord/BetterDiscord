6e13964cacd2eb3b790bf75e139dc87386aef828
Aaron's Development <super.conctacto@gmail.com>
2021-06-06 19:11:51 +0200

    Fix injection in linux

--- injector/src/index.js
+++ injector/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import path from "path";
 import {app} from "electron";
 import Module from "module";
@@ -6,30 +7,38 @@ import ipc from "./modules/ipc";
 import BrowserWindow from "./modules/browserwindow";
 import CSP from "./modules/csp";
 
-if (!process.argv.includes("--vanilla")) {
-    process.env.NODE_OPTIONS = "--no-force-async-hooks-checks";
-    app.commandLine.appendSwitch("no-force-async-hooks-checks");
-    process.electronBinding("command_line").appendSwitch("no-force-async-hooks-checks");
+const log = async (module, content, placements) => {
+    console.log(`[${module}:${placements}] ${content}`);
+};
 
-
-    // Patch and replace the built-in BrowserWindow
-    BrowserWindow.patchBrowserWindow();
-
-    // Register all IPC events
-    ipc.registerEvents();
-
-
-    // Remove CSP immediately on linux since they install to discord_desktop_core still
-    if (process.platform == "win32" || process.platform == "darwin") app.once("ready", CSP.remove);
-    else CSP.remove();
-}
-
-// Use Discord's info to run the app
-if (process.platform == "win32" || process.platform == "darwin") {
+const init = async () => {
     const basePath = path.join(app.getAppPath(), "..", "app.asar");
     const pkg = __non_webpack_require__(path.join(basePath, "package.json"));
+
     app.setAppPath(basePath);
     app.name = pkg.name;
-    Module._load(path.join(basePath, pkg.main), null, true);
-}
 
+    Module._load(path.join(basePath, pkg.main), null, true);
+};
+
+if (!process.argv.includes("--vanilla")) {
+    // Start Discord with injections
+    log("Checker", "Injected correctly. Starting Bootstrapper...", "Init");
+    // [aarons-dev] If you call CSP.remove(); before the app is loaded, it will return an error,
+    // so we do all of that stuff later it does the loading, as it also injects.
+    app.once("ready", () => {
+        process.env.NODE_OPTIONS = "--no-force-async-hooks-checks";
+        process.electronBinding("command_line").appendSwitch("no-force-async-hooks-checks");
+        app.commandLine.appendSwitch("no-force-async-hooks-checks");
+
+        CSP.remove();
+
+        BrowserWindow.patchBrowserWindow();
+        ipc.registerEvents();
+        init();
+    });
+} else {
+    // Start discord without any injection
+    log("BetterDiscord", "Removing injection, discord started in vanilla mode", "Init");
+    init();
+}
