b20814c3491495ccebae6c46a1d64e33996159ba
Zack Rauen <rauenzi@outlook.com>
2021-10-22 16:39:40 -0400

    Release hotfixed change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,18 @@
 
 This changelog starts with the restructured 1.0.0 release that happened after context isolation changes. The changelogs here should more-or-less mirror the ones that get shown in the client but probably with less formatting and pizzazz.
 
+## 1.2.3
+
+### Added
+
+### Removed
+
+### Changed
+
+### Fixed
+- Fixed translations not falling back to English
+- Fixed waiting for guilds due to class name changes
+
 ## 1.2.2
 
 ### Added
--- renderer/src/data/changelog.js
+++ renderer/src/data/changelog.js
@@ -5,13 +5,15 @@ export default {
             title: "New Translations",
             type: "added",
             items: [
-                "Czech, Spanish, Hindi (partial), Italian, Dutch, Norwegian, Portuguese (PT), Romanian, Russian, Turkish"
+                "Czech, Spanish, Hindi (partial), Italian, Dutch, Norwegian (partial), Portuguese (PT), Romanian, Russian, Turkish",
+                "Languages also fallback to English when a translation does not exist."
             ]
         },
         {
             title: "Fixes",
             type: "fixed",
             items: [
+                "Canary loading no longer blocked by changed class names.",
                 "The following were all thanks to Strencher! (https://github.com/Strencher)",
                 "The public servers button is back!",
                 "Guild related classes are back too!",
