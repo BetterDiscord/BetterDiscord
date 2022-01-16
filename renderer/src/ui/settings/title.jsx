9686328361e5a4476cbd2288e7a718eeba27c34c
Strencher <46447572+Strencher@users.noreply.github.com>
2022-01-16 17:09:12 +0100

    Remove old title classNames

--- renderer/src/ui/settings/title.jsx
+++ renderer/src/ui/settings/title.jsx
@@ -1,7 +1,7 @@
 import {React} from "modules";
 
-const className = "bd-settings-title h2-2gWE-o title-3sZWYQ size16-14cGz5 height20-mO2eIN weightSemiBold-NJexzi defaultColor-1_ajX0 defaultMarginh2-2LTaUL marginBottom20-32qID7";
-const className2 = "bd-settings-title bd-settings-group-title h5-18_1nd title-3sZWYQ size12-3R0845 height16-2Lv3qA weightSemiBold-NJexzi da-h5 da-title da-size12 da-height16 da-weightSemiBold marginBottom4-2qk4Hy da-marginBottom4 marginTop8-1DLZ1n da-marginTop8";
+const className = "bd-settings-title";
+const className2 = "bd-settings-title bd-settings-group-title";
 
 export default class SettingsTitle extends React.Component {
     render() {
