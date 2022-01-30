5a087c0f7e34b39243f99dc9dad42279fd82035a
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-01-30 12:24:09 -0600

    update blankstate messages to direct to the store

--- renderer/src/ui/settings/addonlist/installed.jsx
+++ renderer/src/ui/settings/addonlist/installed.jsx
@@ -23,17 +23,9 @@ export default class AddonList extends React.Component {
         this.forceUpdate();
     }
 
-    openFolder = () => {
-        const shell = require("electron").shell;
-        const open = shell.openItem || shell.openPath;
-        open(this.props.folder);
-    }
-
     get emptyImage() {
         const message = Strings.Addons.blankSlateMessage.format({link: `https://betterdiscord.app/${this.props.type}s`, type: this.props.type}).toString();
-        return <EmptyImage title={Strings.Addons.blankSlateHeader.format({type: this.props.type})} message={message}>
-            <button className="bd-button" onClick={this.openFolder}>{Strings.Addons.openFolder.format({type: this.props.type})}</button>
-        </EmptyImage>;
+        return <EmptyImage title={Strings.Addons.blankSlateHeader.format({type: this.props.type})} message={message} />;
     }
 
     render() {
