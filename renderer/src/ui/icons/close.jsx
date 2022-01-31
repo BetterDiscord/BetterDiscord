7f8dd370b051b1ea979fde251866cc3db7c7c541
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-01-30 20:09:07 -0600

    get rid of unnecessary px units in width/height attributes

--- renderer/src/ui/icons/close.jsx
+++ renderer/src/ui/icons/close.jsx
@@ -2,7 +2,7 @@ import {React} from "modules";
 
 export default class CloseButton extends React.Component {
     render() {
-        return <svg {...this.props} width={this.props.size || "24px"} height={this.props.size || "24px"} viewBox="0 0 24 24">
+        return <svg {...this.props} width={this.props.size || "24"} height={this.props.size || "24"} viewBox="0 0 24 24">
             <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path>
         </svg>;
     }
