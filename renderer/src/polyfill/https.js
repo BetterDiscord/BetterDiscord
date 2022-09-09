f35a8a471494a4c226c7afc477505818689e2282
Jasper <40232406+jaspwr@users.noreply.github.com>
2022-09-10 01:51:06 +1000

    Update https.js

--- renderer/src/polyfill/https.js
+++ renderer/src/polyfill/https.js
@@ -11,12 +11,12 @@ export function get(url, options = {}, callback) {
 
     callback(emitter);
 
-    if(!options.encoding)
-        options['encoding'] = "binary";
+    if(options && !options.encoding)
+        options.encoding = "binary";
 
     Remote.https.get(url, options, (error, res, body) => {
         if (error) return emitter.emit("error", error);
-        emitter.emit("data", new Buffer(Buffer.from(body, "binary")));
+        emitter.emit("data", options && options.encoding === "binary" ? new Buffer(Buffer.from(body, "binary")) : body);
         emitter.emit("end", res);
     });
 
