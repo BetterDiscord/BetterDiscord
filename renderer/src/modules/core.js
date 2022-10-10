3e413cfbb0db7403a6cd26fe01a65adba4b9227a
Mopsgamer <kisillev020304@gmail.com>
2022-10-10 22:45:07 +0200

    Broken Updater import fix
    • stepsCount has added

--- renderer/src/modules/core.js
+++ renderer/src/modules/core.js
@@ -15,7 +15,7 @@ import IPC from "./ipc";
 import LoadingInterface from "../loading";
 import Styles from "../styles/index.css";
 import Editor from "./editor";
-import Updater from "./updater";
+import {PluginUpdater, ThemeUpdater, CoreUpdater} from "./updater";
 
 export default new class Core {
     async startup() {
@@ -27,60 +27,65 @@ export default new class Core {
         Config.dataPath = process.env.BETTERDISCORD_DATA_PATH;
 
         /**loading steps count*/
+        const stepsCount = 14;
 
         // Load css early
         Logger.log("Startup", "Injecting BD Styles");
         DOMManager.injectStyle("bd-stylesheet", Styles.toString());
-        await LoadingInterface.setInitStatus(1/15*100, "Injecting BD Styles");
+        await LoadingInterface.setInitStatus(1/stepsCount*100, "Injecting BD Styles");
         
         Logger.log("Startup", "Initializing DataStore");
         DataStore.initialize();
-        await LoadingInterface.setInitStatus(2/15*100, "Initializing DataStore");
+        await LoadingInterface.setInitStatus(2/stepsCount*100, "Initializing DataStore");
         
         Logger.log("Startup", "Initializing LocaleManager");
         LocaleManager.initialize();
-		await LoadingInterface.setInitStatus(3/15*100, "Initializing LocaleManager");
+		await LoadingInterface.setInitStatus(3/stepsCount*100, "Initializing LocaleManager");
         
         Logger.log("Startup", "Getting update information");
-        this.checkForUpdate();
-        await LoadingInterface.setInitStatus(4/15*100, "Getting update information");
+        CoreUpdater.checkForUpdate()
+        await LoadingInterface.setInitStatus(4/stepsCount*100, "Getting update information");
 				
         Logger.log("Startup", "Initializing Settings");
         Settings.initialize();
-        await LoadingInterface.setInitStatus(5/15*100, "Initializing Settings");
+        await LoadingInterface.setInitStatus(5/stepsCount*100, "Initializing Settings");
         
         Logger.log("Startup", "Initializing DOMManager");
         DOMManager.initialize();
-        await LoadingInterface.setInitStatus(6/15*100, "Initializing DOMManager");
+        await LoadingInterface.setInitStatus(6/stepsCount*100, "Initializing DOMManager");
         
         Logger.log("Startup", "Waiting for connection...");
         await this.waitForConnection();
-        await LoadingInterface.setInitStatus(7/15*100, "Waiting for connection...");
+        await LoadingInterface.setInitStatus(7/stepsCount*100, "Waiting for connection...");
         
         Logger.log("Startup", "Initializing Editor");
         await Editor.initialize();
-        await LoadingInterface.setInitStatus(8/15*100, "Initializing Editor");
+        await LoadingInterface.setInitStatus(8/stepsCount*100, "Initializing Editor");
 				
         Logger.log("Startup", "Initializing Builtins");
 	    Modals.initialize();
         for (const module in Builtins) {
             Builtins[module].initialize();
         }
-        await LoadingInterface.setInitStatus(9/15*100, "Initializing Builtins");
+        await LoadingInterface.setInitStatus(9/stepsCount*100, "Initializing Builtins");
         
         Logger.log("Startup", "Loading Plugins");
         // const pluginErrors = [];
         const pluginErrors = PluginManager.initialize();
-        await LoadingInterface.setInitStatus(10/15*100, "Loading Plugins");
+        await LoadingInterface.setInitStatus(10/stepsCount*100, "Loading Plugins");
         
         Logger.log("Startup", "Loading Themes");
         // const themeErrors = [];
         const themeErrors = ThemeManager.initialize();
-        await LoadingInterface.setInitStatus(11/15*100, "Loading Themes");
+        await LoadingInterface.setInitStatus(11/stepsCount*100, "Loading Themes");
         
-        Logger.log("Startup", "Initializing AddonUpdater");
-        AddonUpdater.initialize();
-        await LoadingInterface.setInitStatus(12/15*100, "Initializing AddonUpdater");
+        Logger.log("Startup", "Initializing PluginUpdater");
+        PluginUpdater.initialize();
+        await LoadingInterface.setInitStatus(12/stepsCount*100, "Initializing ThemeUpdater");
+
+        Logger.log("Startup", "Initializing ThemeUpdater");
+        ThemeUpdater.initialize();
+        await LoadingInterface.setInitStatus(13/stepsCount*100, "Initializing ThemeUpdater");
         
         Logger.log("Startup", "Removing Loading Interface");
         LoadingInterface.hide();
