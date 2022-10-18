2b2bdc88f26cf971abc16d27546d06fbe32145e7
FedeIlLeone <dileopolus@virgilio.it>
2022-10-18 21:54:52 +0200

    Fix public server verified badge classes

--- renderer/src/ui/publicservers/card.jsx
+++ renderer/src/ui/publicservers/card.jsx
@@ -1,15 +1,15 @@
 import {React, Strings} from "modules";
 
-const badge = <div className="flowerStarContainer-3zDVtj verified-1eC5dy background-2uufRq guildBadge-RlDbED"
+const badge = <div className="flowerStarContainer-1QeD-L verified-1Jv_7P background-3Da2vZ guildBadge-U9DBt_"
 style={{width: "16px", height: "16px"}}>
-<svg aria-label="Verified &amp; Partnered" className="flowerStar-1GeTsn"
+<svg aria-label="Verified &amp; Partnered" className="flowerStar-2tNFCR"
     aria-hidden="false" width="16" height="16" viewBox="0 0 16 15.2">
     <path fill="currentColor" fillRule="evenodd"
         d="m16 7.6c0 .79-1.28 1.38-1.52 2.09s.44 2 0 2.59-1.84.35-2.46.8-.79 1.84-1.54 2.09-1.67-.8-2.47-.8-1.75 1-2.47.8-.92-1.64-1.54-2.09-2-.18-2.46-.8.23-1.84 0-2.59-1.54-1.3-1.54-2.09 1.28-1.38 1.52-2.09-.44-2 0-2.59 1.85-.35 2.48-.8.78-1.84 1.53-2.12 1.67.83 2.47.83 1.75-1 2.47-.8.91 1.64 1.53 2.09 2 .18 2.46.8-.23 1.84 0 2.59 1.54 1.3 1.54 2.09z">
     </path>
 </svg>
-<div className="childContainer-1wxZNh">
-    <svg className="icon-1ihkOt" aria-hidden="false" width="16" height="16" viewBox="0 0 16 15.2">
+<div className="childContainer-U_a6Yh">
+    <svg className="icon-3BYlXK" aria-hidden="false" width="16" height="16" viewBox="0 0 16 15.2">
         <path d="M7.4,11.17,4,8.62,5,7.26l2,1.53L10.64,4l1.36,1Z" fill="currentColor"></path>
     </svg>
 </div>
