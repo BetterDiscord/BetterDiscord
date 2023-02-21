99f8bc29a83d4f8449b0a2d9d0182b101efb09b3
Strencher <46447572+Strencher@users.noreply.github.com>
2023-02-21 23:11:59 +0100

    Swap to standalone header (#1530)
    * Lazy load sidebar header
    
    * Publish CSS
    
    * Swap to standalone header

--- renderer/src/ui/settings/sidebarheader.jsx
+++ renderer/src/ui/settings/sidebarheader.jsx
@@ -3,13 +3,16 @@ import {React, WebpackModules} from "modules";
 import HistoryIcon from "../icons/history";
 import Modals from "../modals";
 
-const SidebarComponents = WebpackModules.getModule(m => m.Header && m.Separator && m.Item);
 const Tooltip = WebpackModules.getByPrototypes("renderTooltip");
 
 export default class SettingsTitle extends React.Component {
+    renderHeader() {
+        return <h2 className="bd-sidebar-header-label">BetterDiscord</h2>;
+    }
+
     render() {
         return <div className="bd-sidebar-header">
-                    <SidebarComponents.Header>BetterDiscord</SidebarComponents.Header>
+                    {this.renderHeader()}
                     <Tooltip color="primary" position="top" text="Changelog">
                         {props =>
                             <div {...props} className="bd-changelog-button" onClick={() => Modals.showChangelogModal(Changelog)}>
