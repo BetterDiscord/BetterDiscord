c6d58bdc2d5b27ff78790f44231357a09f77c56e
Zack Rauen <rauenzi@outlook.com>
2023-03-06 02:16:00 -0500

    Convert icons to functional components

--- renderer/src/ui/icons/check.jsx
+++ renderer/src/ui/icons/check.jsx
@@ -1,11 +1,9 @@
 import {React} from "modules";
 
-export default class Checkmark extends React.Component {
-    render() {
-        const size = this.props.size || "24px";
-        return <svg viewBox="0 0 24 24" fill="#FFFFFF" className={this.props.className || ""} style={{width: size, height: size}} onClick={this.props.onClick}>
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-            </svg>;
-    }
+export default function Checkmark(props) {
+    const size = props.size || "24px";
+    return <svg viewBox="0 0 24 24" fill="#FFFFFF" className={props.className || ""} style={{width: size, height: size}} onClick={props.onClick}>
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+        </svg>;
 }
