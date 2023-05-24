f629f0a851ac9bda0bfc8be7473fac15378595f5
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2023-05-24 11:54:04 -0700

    Fix window size retention (#1612)

--- renderer/src/builtins/window/removeminimumsize.js
+++ renderer/src/builtins/window/removeminimumsize.js
@@ -15,7 +15,7 @@ export default new class RemoveMinimumSize extends Builtin {
         window.addEventListener("resize", this.onResize);
 
         const winprefs = DataStore.getData("windowprefs");
-        if (!winprefs.height || !winprefs.width) return; // If the values don't exist exit
+        if (!winprefs.height || !winprefs.width) return DataStore.setData("windowprefs", {}); // If the values don't exist exit and initialize
         if ((winprefs.height >= DISCORD_MIN_HEIGHT) && (winprefs.width >= DISCORD_MIN_WIDTH)) return; // If both values are normally valid don't touch
         IPC.setWindowSize(winprefs.width, winprefs.height);
     }
