b645c53aee599e0b4f1b3cf6f3931ba70fd95662
Zack Rauen <rauenzi@outlook.com>
2023-08-29 00:33:24 -0400

    Remove outdated strings

--- renderer/src/ui/settings/sidebarheader.jsx
+++ renderer/src/ui/settings/sidebarheader.jsx
@@ -2,6 +2,7 @@ import Changelog from "@data/changelog";
 
 import React from "@modules/react";
 import DiscordModules from "@modules/discordmodules";
+import Strings from "@modules/strings";
 
 import HistoryIcon from "@ui/icons/history";
 
@@ -11,7 +12,7 @@ import Modals from "@ui/modals";
 export default function SettingsTitle() {
     return <div className="bd-sidebar-header">
                 <h2 className="bd-sidebar-header-label">BetterDiscord</h2>
-                <DiscordModules.Tooltip color="primary" position="top" text="Changelog">
+                <DiscordModules.Tooltip color="primary" position="top" text={Strings.Modals.changelog}>
                     {props =>
                         <div {...props} className="bd-changelog-button" onClick={() => Modals.showChangelogModal(Changelog)}>
                             <HistoryIcon className="bd-icon" size="16px" />
