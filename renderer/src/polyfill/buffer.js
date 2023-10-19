f77110affeb1a2bcdc80e420ea75a96d9dce10ea
Zack Rauen <rauenzi@outlook.com>
2023-10-19 05:27:29 -0400

    Add deprecation notice

--- renderer/src/polyfill/buffer.js
+++ renderer/src/polyfill/buffer.js
@@ -1,5 +1,11 @@
+import Logger from "@common/logger";
+
+
 Object.defineProperty(window, "Buffer", {
-    get() {return Buffer;},
+    get() {
+        Logger.warn("Deprecated", `Usage of the Buffer global is deprecated. Consider using web standards such as Uint8Array and TextDecoder/TextEncoder.`);
+        return Buffer;
+    },
     configurable: true,
     enumerable: false
 });
