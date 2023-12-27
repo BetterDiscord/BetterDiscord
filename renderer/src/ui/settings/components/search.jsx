fd218d2c60e3b33fd3acfe8e62da62427918cdf9
flatypus [mac] <yhc3141@gmail.com>
2023-12-27 02:50:18 -0800

    Auto focus search bar when plugins/themes tab is selected #1213

--- renderer/src/ui/settings/components/search.jsx
+++ renderer/src/ui/settings/components/search.jsx
@@ -1,19 +1,26 @@
 import React from "@modules/react";
 import SearchIcon from "@ui/icons/search";
 
-const {useState, useCallback} = React;
+const {useState, useEffect, useCallback, useRef} = React;
 
 
 export default function Search({onChange, className, onKeyDown, placeholder}) {
+    const input = useRef(null);
     const [value, setValue] = useState("");
+
+    // focus search bar on page select
+    useEffect(()=>{
+        if (!input.current) return;
+        input.current.focus();
+    }, []);
+
     const change = useCallback((e) => {
         onChange?.(e);
         setValue(e.target.value);
     }, [onChange]);
 
-
     return <div className={"bd-search-wrapper" + (className ? ` ${className}` : "")}>
-                <input onChange={change} onKeyDown={onKeyDown} type="text" className="bd-search" placeholder={placeholder} maxLength="50" value={value} />
+                <input onChange={change} onKeyDown={onKeyDown} type="text" className="bd-search" placeholder={placeholder} maxLength="50" value={value} ref={input}/>
                 <SearchIcon />
             </div>;
 
