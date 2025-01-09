1b428cc0aabe0d2d410aedae472f94d6e24046a5
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-01-08 18:40:59 -0600

    i18n + resize

--- renderer/src/builtins/developer/recovery.js
+++ renderer/src/builtins/developer/recovery.js
@@ -3,6 +3,8 @@ import Patcher from "@modules/patcher";
 import Button from "@ui/base/button";
 import React from "@modules/react";
 import Logger from "@common/logger";
+import discordmodules from "@modules/discordmodules";
+import Strings from "@modules/strings";
 
 const ErrorDetails = ({componentStack}) => {
     const [isExpanded, setIsExpanded] = React.useState(false);
@@ -10,33 +12,37 @@ const ErrorDetails = ({componentStack}) => {
     const contentRef = React.useRef(null);
 
     React.useEffect(() => {
-        if (isExpanded) {
-            const contentHeight = "534";
-            setHeight(contentHeight);
-        }
+        const updateHeight = () => {
+            if (isExpanded) {
+                const maxHeight = Math.min(0.6 * window.innerHeight, 534);
+                setHeight(maxHeight);
+            }
  else {
-            setHeight(0);
-        }
+                setHeight(0);
+            }
+        };
+
+        updateHeight();
+        window.addEventListener("resize", updateHeight);
+
+        return () => window.removeEventListener("resize", updateHeight);
     }, [isExpanded]);
 
     return (
         <div className="bd-error-container">
             <div className="bd-error-toggle-wrapper">
-                <Button 
+                <Button
                     className={`bd-error-toggle ${isExpanded ? "expanded" : ""}`}
                     onClick={() => setIsExpanded(!isExpanded)}
                 >
                     {isExpanded ? "Hide Error Details ▼" : "Show Error Details ▶"}
                 </Button>
             </div>
-            <div 
+            <div
                 className="bd-error-content-wrapper"
-                style={{height: `${height}px`}}
+                style={{height: `${height}px`, overflow: "hidden"}}
             >
-                <div 
-                    ref={contentRef} 
-                    className="bd-error-content"
-                >
+                <div ref={contentRef} className="bd-error-content">
                     {componentStack}
                 </div>
             </div>
@@ -48,21 +54,19 @@ export default class Recovery {
     static initialize() {
         this.patchErrorBoundry();
         this.parseModule = Webpack.getByKeys("defaultRules", "parse");
-        this.buttonSizes = Webpack.getByKeys("sizeLarge");
-        this.dispatch = Webpack.getByKeys("_dispatch", "subscribe");
         this.routeModule = Webpack.getByStrings("transitionTo", {searchExports: true});
     }
 
     static attemptRecovery() {
         try {
-            this.dispatch?.dispatch?.({
+            discordmodules?.dispatch?.({
                 type: "LAYER_POP_ALL"
             });
         }
         catch (error) {
             Logger.error("Failed to pop all layers:", error);
         }
-    
+
         try {
             this.dispatch?.dispatch?.({
                 type: "MODAL_POP_ALL"
@@ -71,7 +75,7 @@ export default class Recovery {
         catch (error) {
             Logger.error("Failed to pop all modals:", error);
         }
-    
+
         try {
             this?.routeModule?.("/channels/@me");
         }
@@ -79,7 +83,7 @@ export default class Recovery {
             Logger.error("Failed to route to main channel:", error);
         }
     }
-    
+
     static patchErrorBoundry() {
         const [mod] = Webpack.getWithKey(Webpack.Filters.byStrings(":this._handleSubmitReport,children:"));
 
@@ -92,14 +96,14 @@ export default class Recovery {
             const parsedError = errorStack ? this.parseModule.parse(`\`\`\`${errorStack}\`\`\``) : null;
 
             buttons.children.push(
-                <Button 
-                    className={`bd-button-recovery ${this.buttonSizes.sizeLarge}`} 
+                <Button
+                    className={`bd-button-recovery`}
                     onClick={() => {
                         this.attemptRecovery();
                         instance.setState({info: null, error: null});
                     }}
                 >
-                    Attempt To Recover Discord
+                    {Strings.Misc.recover}
                 </Button>,
                 parsedError && <ErrorDetails componentStack={parsedError} />
             );
