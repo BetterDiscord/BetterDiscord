863ee79471089e46f4709f59d6c7d5844a6cea69
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-02-05 20:06:37 -0600

    whoops

--- src/betterdiscord/api/reactutils.ts
+++ src/betterdiscord/api/reactutils.ts
@@ -80,6 +80,13 @@ interface ReactUtils {
     ): React.FunctionComponent<P>;
 }
 
+/**
+ * `ReactUtils` is a utility class for interacting with React internals. Instance is accessible through the {@link BdApi}.
+ * This is extremely useful for interacting with the internals of the UI.
+ * @type ReactUtils
+ * @summary {@link ReactUtils} is a utility class for interacting with React internals.
+ * @name ReactUtils
+ */
 const ReactUtils: ReactUtils = {
     get rootInstance() {
         return document.getElementById("app-mount")?._reactRootContainer?._internalRoot?.current;
