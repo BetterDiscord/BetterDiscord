55cbfe16d2457021b6e772bffc457d53b6a09efa
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-06-03 13:25:50 +0200

    add plugin & lint fix

--- src/betterdiscord/stores/base.ts
+++ src/betterdiscord/stores/base.ts
@@ -5,12 +5,12 @@ export default abstract class Store {
           if (Store.prototype.getName.call(store) === name) return store as T;
         }
     }
-    
-    
+
+
     constructor() {
         Store.stores.add(this);
     }
-    
+
     initialize(): void {};
 
     displayName?: string;
@@ -18,8 +18,8 @@ export default abstract class Store {
         if (this.displayName) return this.displayName;
         return this.constructor.name;
     }
-    
-      
+
+
     #listeners = new Set<() => void>();
     addChangeListener(callback: () => void) {
         this.#listeners.add(callback);
@@ -29,7 +29,7 @@ export default abstract class Store {
     removeChangeListener(callback: () => void) {
         this.#listeners.delete(callback);
     }
-    
+
     emit() {
         for (const listener of this.#listeners) {
             listener();
