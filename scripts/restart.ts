1aca3936671e4e87cde8c3e60a333a42baa8bf60
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-06-03 17:26:43 +0200

    Refactor restart script to TypeScript and update package.json

--- scripts/restart.ts
+++ scripts/restart.ts
@@ -1,7 +1,7 @@
 const args = process.argv;
-const fs = require("fs");
-const path = require("path");
-const {execSync, spawn} = require("child_process");
+import fs from "fs";
+import path from "path";
+import {execSync, spawn} from "child_process";
 
 const useBdRelease = args[2] && args[2].toLowerCase() === "release";
 const releaseInput = useBdRelease ? args[3] && args[3].toLowerCase() : args[2] && args[2].toLowerCase();
@@ -12,7 +12,7 @@ if (process.platform === "win32") discordExeName += ".exe";
 const discordPath = (function () {
     let latestExe = "";
     if (process.platform === "win32") {
-        const basedir = path.join(process.env.LOCALAPPDATA, release.replace(/ /g, ""));
+        const basedir = path.join(process.env.LOCALAPPDATA!, release.replace(/ /g, ""));
         if (!fs.existsSync(basedir)) throw new Error(`Cannot find directory for ${release}`);
         const version = fs.readdirSync(basedir).filter(f => fs.lstatSync(path.join(basedir, f)).isDirectory() && f.split(".").length > 1).sort().reverse()[0];
         latestExe = path.join(basedir, version, discordExeName);
@@ -34,12 +34,13 @@ console.log("");
 console.log(`Stopping ${release}`);
 const killCommand = process.platform === "win32" ? "taskkill /F /IM " + discordExeName : "pkill -f " + discordExeName;
 try {
-    execSync(killCommand, {shell: false, stdio: "ignore"});
+    execSync(killCommand, {stdio: "ignore"});
     console.log(`    ✅ Stopped ${release}`);
 }
 catch (error) {
-    if (error.status !== 128) console.log(`    ❌ Failed to stop ${release}`);
-    console.log(`    ❌ Failed to stop ${release}, but it may not be running`);
+    const status = (error as {status?: number;}).status;
+    if (status === 128 && process.platform === "win32" || status === 1) console.log(`    ☑️ ${release} wasn't running`);
+    console.log(`    ❌ Failed to stop ${release}`);
 }
 console.log("");
 
