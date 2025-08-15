61a1345b51316f0142816e000df6acf58ea1b7be
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-08-14 20:10:10 -0500

    Update notifyAddonUpdater.ts

--- src/betterdiscord/builtins/general/notifyAddonUpdater.ts
+++ src/betterdiscord/builtins/general/notifyAddonUpdater.ts
@@ -2,6 +2,6 @@ import Builtin from "@structs/builtin";
 
 export default new class NotifyAddonUpdate extends Builtin {
     get name() {return "NotifyAddonUpdate";}
-    get category() {return "developer";}
+    get category() {return "general";}
     get id() {return "notifyAddonUpdate";}
 };
