ee26064381d6ec38e0d06309f59b55eb9bf15ca8
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-08-14 20:11:34 -0500

    Delete notifyAddonUpdater.ts

--- src/betterdiscord/builtins/general/notifyAddonUpdater.ts
+++
@@ -1,7 +0,0 @@
-import Builtin from "@structs/builtin";
-
-export default new class NotifyAddonUpdate extends Builtin {
-    get name() {return "NotifyAddonUpdate";}
-    get category() {return "general";}
-    get id() {return "notifyAddonUpdate";}
-};
