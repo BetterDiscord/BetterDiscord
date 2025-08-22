dc9dd774998d974716261cea7affa809dff7729a
Zerebos <me@zerebos.com>
2025-08-22 14:36:54 -0400

    Update version

--- renderer/src/data/changelog.js
+++ renderer/src/data/changelog.js
@@ -6,16 +6,13 @@ export default {
     subtitle: `v${config.version}`,
     video: "https://www.youtube.com/embed/jDclfjPc3k0?si=YlQwrCHnoYkTfa6G&vq=hd720p&hd=1&rel=0&showinfo=0&mute=0&loop=1&autohide=1",
     // banner: "https://i.imgur.com/wuh5yMK.png",
-    blurb: "This should hopefully be the last update before the next major release. It contains a few fixes and improvements to the current version of BetterDiscord.",
+    blurb: "This is a hotfix for the latest Discord changes.",
     changes: [
         {
             title: "Hot Hot Hotfixes 🔥",
             type: "fixed",
             items: [
-                "Theme attributes are now correctly applied to tabs",
-                "Remove minimum size setting now works correctly.",
-                "General styling around the BetterDiscord settings page has been improved.",
-                "Slash commands now work correctly without crashing."
+                "Settings panel should now be fully functional again.",
             ]
         },
     ]
