f3e01df1f848d749d90a78832699ff7e06e7e2bb
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2025-08-26 08:44:14 -0700

    Fix for editor focus being lost in settings (#1984)
    * Fix Editor Focus Issue
    
    Also fixes onClose for the floating window api
    
    * Forgot to remove the comments

--- src/betterdiscord/ui/floatingwindows.tsx
+++ src/betterdiscord/ui/floatingwindows.tsx
@@ -5,9 +5,10 @@ import DOMManager from "@modules/dommanager";
 
 import FloatingWindowContainer from "./floating/container";
 import {getByDisplayName} from "@webpack";
+import type {FloatingWindowProps} from "./floating/window";
 
 
-const AppLayerProvider = getByDisplayName("AppLayerProvider");
+const AppLayerProvider = getByDisplayName<any>("AppLayerProvider");
 
 let hasInitialized = false;
 export default class FloatingWindows {
@@ -16,14 +17,14 @@ export default class FloatingWindows {
         const wrapped = AppLayerProvider
             ? React.createElement(AppLayerProvider().props.layerContext.Provider, {value: [document.querySelector("#app-mount > .layerContainer-2v_Sit")]}, container)
             : container;
-        const div = DOMManager.parseHTML(`<div id="floating-windows-layer">`);
+        const div = DOMManager.parseHTML(`<div id="floating-windows-layer">`) as HTMLDivElement;
         DOMManager.bdBody.append(div);
         const root = ReactDOM.createRoot(div);
         root.render(wrapped);
         hasInitialized = true;
     }
 
-    static open(window) {
+    static open(window: FloatingWindowProps) {
         if (!hasInitialized) this.initialize();
         return Events.emit("open-window", window);
     }
