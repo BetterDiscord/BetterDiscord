964460a8eaae3c34b5089908a4803cb1afef4ef3
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2025-08-27 15:35:04 -0400

    Lazy-load monaco editor (#2000)
    * Lazy load monaco
    
    * lint
    
    * Remove promise wrapper in loadMonaco

--- src/betterdiscord/modules/editor.ts
+++ src/betterdiscord/modules/editor.ts
@@ -4,6 +4,8 @@ import DOMManager from "./dommanager";
 import {getAllModules, webpackRequire} from "@webpack";
 import Patcher from "./patcher";
 
+import type Monaco from "monaco-editor";
+
 // List of all global classNames (from the app helmet stuff)
 const knownGlobalClasses = [
     "theme-dark",
@@ -64,7 +66,18 @@ interface TextAreaInputControllerType {
 }
 
 export default new class Editor {
+    initPromise: Promise<void> | null = null;
+    failedToLoad = false;
+
     async initialize() {
+        if (this.initPromise) return this.initPromise;
+
+        this.initPromise = this.loadMonaco();
+        return this.initPromise;
+    }
+
+    async loadMonaco() {
+        Logger.log("Editor", "Loading Monaco Editor");
         const baseUrl = `https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/${process.env.__MONACO_VERSION__}/min`;
 
         Object.defineProperty(window, "MonacoEnvironment", {
@@ -95,135 +108,137 @@ export default new class Editor {
 
             // Configure Monaco's AMD loader
             amdLoader.config({paths: {vs: `${baseUrl}/vs`}});
-            amdLoader(["vs/editor/editor.main"], (monaco: typeof import("monaco-editor")) => {
-                const seenIds: Record<PropertyKey, boolean> = {};
-                let size = 0;
-
-                const getSuggestions = () => {
-                    const modules = getAllModules<Array<Record<string, string>>>((exports, module) => {
-                        let isGood = seenIds[module.id];
-
-                        if (typeof isGood === "undefined") {
-                            const key = Object.keys(exports)[0];
-                            isGood = typeof exports[key] === "string" && exports[key].startsWith(`${key}_`);
-
-                            seenIds[module.id] = isGood;
-                        }
-
-                        return isGood;
-                    }, {
-                        searchDefault: false
-                    });
-
-                    const classes = new Set(modules.flatMap((classNames) => Object.values(classNames).flatMap(m => String(m).split(" "))));
-
-                    for (let index = 0; index < knownGlobalClasses.length; index++) {
-                        classes.add(knownGlobalClasses[index]);
-                    }
-
-                    return classes;
-                };
-
-                let suggestions: Set<string>;
-                monaco.languages.registerCompletionItemProvider("css", {
-                    provideCompletionItems: (model, position) => {
-                        // Get text before cursor
-                        const textBeforeCursor = model.getValueInRange(new monaco.Range(position.lineNumber, 1, position.lineNumber, position.column));
-
-                        // Ensure we are inside a selector context (not inside properties)
-                        if (!textBeforeCursor.match(/^\s*\.[\w-]*$/)) {
-                            return {suggestions: []}; // Don't show suggestions inside properties or elsewhere
-                        }
-
-                        const newSize = Object.keys(webpackRequire).length;
-                        if (newSize !== size) {
-                            suggestions = getSuggestions();
-                            size = newSize;
-                        }
-
-                        const classes = new Set(suggestions);
-                        for (const element of (document.all ?? document.querySelectorAll("*"))) {
-                            if (element instanceof Element) {
-                                const classList = element.classList.value.split(" ");
-
-                                for (let index = 0; index < classList.length; index++) {
-                                    classes.add(classList[index]);
-                                }
-                            }
-                        }
-
-                        classes.delete("");
-
-                        const suggestionsRange = monaco.Range.fromPositions(position);
-
-                        return {
-                            suggestions: Array.from(classes, (className) => ({
-                                label: `.${className}`,
-                                kind: monaco.languages.CompletionItemKind.Class,
-                                insertText: className,
-                                range: suggestionsRange
-                            }))
-                        };
-                    }
-                });
-
-                amdLoader(["vs/platform/clipboard/browser/clipboardService"], ({BrowserClipboardService}: {BrowserClipboardService: BrowserClipboardServiceType;}) => {
-                    Patcher.instead("monaco-editor", BrowserClipboardService.prototype, "readText", (that: any, [type]: [type?: string], original: (t?: string) => void) => {
-                        if (type) {
-                            return original.call(that, type);
-                        }
-
-                        return Promise.resolve(DiscordNative.clipboard.read());
-                    });
-                });
-                amdLoader(["vs/editor/browser/controller/textAreaInput"], ({TextAreaWrapper}: {TextAreaWrapper: TextAreaInputControllerType;}) => {
-                    Patcher.instead("monaco-editor", TextAreaWrapper.prototype, "setSelectionRange", (that: any, args, original) => {
-                        const domNode = (that as TextAreaInputControllerType["prototype"])._actual;
-
-                        const undo = Patcher.instead("monaco-editor", HTMLElement.prototype, "focus", (node, _args, focus) => {
-                            if (node === domNode) {
-                                return focus.apply(node);
-                            }
-                        });
-
-                        const ret = original.apply(that, args);
-
-                        undo!();
-
-                        return ret;
-                    });
-                });
-
-                // JS
-                monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
-                    noSemanticValidation: true,
-                    noSyntaxValidation: false
-                });
-                monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-                    target: monaco.languages.typescript.ScriptTarget.ESNext,
-                    allowNonTsExtensions: true
-                });
-
-                // const libSource = `
-                //     interface Webpack {}
-
-                //     declare class BdApi {
-                //         constructor(name: string) {}
-
-                //         Webpack!: Webpack;
-                //         static Webpack!: Webpack;
-                //     }
-                // `;
-
-                // const libUri = "ts:filename/bdapi.d.ts";
-                // monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
-                // // When resolving definitions and references, the editor will try to use created models.
-                // // Creating a model for the library allows "peek definition/references" commands to work with the library.
-                // monaco.editor.createModel(libSource, "typescript", monaco.Uri.parse(libUri));
+            const monaco = await new Promise<typeof Monaco>((res) => {
+                amdLoader(["vs/editor/editor.main"], res);
             });
+            const seenIds: Record<PropertyKey, boolean> = {};
+            let size = 0;
+
+            const getSuggestions = () => {
+                const modules = getAllModules<Array<Record<string, string>>>((exports, module) => {
+                    let isGood = seenIds[module.id];
+
+                    if (typeof isGood === "undefined") {
+                        const key = Object.keys(exports)[0];
+                        isGood = typeof exports[key] === "string" && exports[key].startsWith(`${key}_`);
+
+                        seenIds[module.id] = isGood;
+                    }
+
+                    return isGood;
+                }, {
+                    searchDefault: false
+                });
+
+                const classes = new Set(modules.flatMap((classNames) => Object.values(classNames).flatMap(m => String(m).split(" "))));
+
+                for (let index = 0; index < knownGlobalClasses.length; index++) {
+                    classes.add(knownGlobalClasses[index]);
+                }
+
+                return classes;
+            };
+
+            let suggestions: Set<string>;
+            monaco.languages.registerCompletionItemProvider("css", {
+                provideCompletionItems: (model, position) => {
+                    // Get text before cursor
+                    const textBeforeCursor = model.getValueInRange(new monaco.Range(position.lineNumber, 1, position.lineNumber, position.column));
+
+                    // Ensure we are inside a selector context (not inside properties)
+                    if (!textBeforeCursor.match(/^\s*\.[\w-]*$/)) {
+                        return {suggestions: []}; // Don't show suggestions inside properties or elsewhere
+                    }
+
+                    const newSize = Object.keys(webpackRequire).length;
+                    if (newSize !== size) {
+                        suggestions = getSuggestions();
+                        size = newSize;
+                    }
+
+                    const classes = new Set(suggestions);
+                    for (const element of (document.all ?? document.querySelectorAll("*"))) {
+                        if (element instanceof Element) {
+                            const classList = element.classList.value.split(" ");
+
+                            for (let index = 0; index < classList.length; index++) {
+                                classes.add(classList[index]);
+                            }
+                        }
+                    }
+
+                    classes.delete("");
+
+                    const suggestionsRange = monaco.Range.fromPositions(position);
+
+                    return {
+                        suggestions: Array.from(classes, (className) => ({
+                            label: `.${className}`,
+                            kind: monaco.languages.CompletionItemKind.Class,
+                            insertText: className,
+                            range: suggestionsRange
+                        }))
+                    };
+                }
+            });
+
+            amdLoader(["vs/platform/clipboard/browser/clipboardService"], ({BrowserClipboardService}: {BrowserClipboardService: BrowserClipboardServiceType;}) => {
+                Patcher.instead("monaco-editor", BrowserClipboardService.prototype, "readText", (that: any, [type]: [type?: string], original: (t?: string) => void) => {
+                    if (type) {
+                        return original.call(that, type);
+                    }
+
+                    return Promise.resolve(DiscordNative.clipboard.read());
+                });
+            });
+            amdLoader(["vs/editor/browser/controller/textAreaInput"], ({TextAreaWrapper}: {TextAreaWrapper: TextAreaInputControllerType;}) => {
+                Patcher.instead("monaco-editor", TextAreaWrapper.prototype, "setSelectionRange", (that: any, args, original) => {
+                    const domNode = (that as TextAreaInputControllerType["prototype"])._actual;
+
+                    const undo = Patcher.instead("monaco-editor", HTMLElement.prototype, "focus", (node, _args, focus) => {
+                        if (node === domNode) {
+                            return focus.apply(node);
+                        }
+                    });
+
+                    const ret = original.apply(that, args);
+
+                    undo!();
+
+                    return ret;
+                });
+            });
+
+            // JS
+            monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+                noSemanticValidation: true,
+                noSyntaxValidation: false
+            });
+            monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+                target: monaco.languages.typescript.ScriptTarget.ESNext,
+                allowNonTsExtensions: true
+            });
+
+            // const libSource = `
+            //     interface Webpack {}
+
+            //     declare class BdApi {
+            //         constructor(name: string) {}
+
+            //         Webpack!: Webpack;
+            //         static Webpack!: Webpack;
+            //     }
+            // `;
+
+            // const libUri = "ts:filename/bdapi.d.ts";
+            // monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
+            // // When resolving definitions and references, the editor will try to use created models.
+            // // Creating a model for the library allows "peek definition/references" commands to work with the library.
+            // monaco.editor.createModel(libSource, "typescript", monaco.Uri.parse(libUri));
         }
         catch (e) {
             Logger.error("Editor", "Failed to load monaco editor", e);
+            this.failedToLoad = true;
         }
         finally {
             // Revert the global require to CommonJS
