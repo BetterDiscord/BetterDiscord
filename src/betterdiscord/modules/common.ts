dd74cd69e3dd778997c88da07c32110f80858e22
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-08-27 15:04:01 -0500

    Update common.ts

--- src/betterdiscord/modules/common.ts
+++ src/betterdiscord/modules/common.ts
@@ -1,6 +1,10 @@
 import * as Webpack from "@webpack";
 
-const [FluxDispatch, Parse, TextInput, Popout, MessageActions, Clickable, Slider, Modal, CloudUpload, useStateFromStores, moment, hljs, Snowflake, Tooltip, lodash, cssVars, fetchUser] = Webpack.getBulk(
+export function sleep(delay: number) {
+    return new Promise<void>(done => setTimeout(() => done(), delay));
+}
+
+const [FluxDispatch, Parse, TextInput, Popout, MessageActions, Clickable, Slider, Modal, CloudUpload, useStateFromStores, moment, hljs, Snowflake, Tooltip, lodash, cssVars, fetchUser, Intl, Flux, permissions, ComponentDispatch, FormNotice, ColorPicker] = Webpack.getBulk(
     {filter: x => x._dispatch}, // flux
     {filter: x => x.parseTopic}, // parser
     {filter: Webpack.Filters.byStrings('bmQU//', '+DFxLS'), searchExports: true}, // textnput
@@ -8,7 +12,7 @@ const [FluxDispatch, Parse, TextInput, Popout, MessageActions, Clickable, Slider
     {filter: Webpack.Filters.byKeys(["sendMessage", "editMessage"])}, // actions
     {filter: Webpack.Filters.byStrings('renderNonInteractive()'), searchExports: true}, // clickable
     {filter: Webpack.Filters.byStrings("markerPositions"), searchExports: true}, // slider
-    {filter: Webpack.Filters.byStrings('["size","title"')}, // modal
+    {filter: Webpack.Filters.byStrings('["size","title"'), searchExports: true}, // modal
     {filter: Webpack.Filters.byStrings("uploadFileToCloud"), searchExports: true}, // cloud uploader
     {filter: Webpack.Filters.byStrings('useStateFromStores'), searchExports: true}, // states
     {filter: x => x?.parseTwoDigitYear}, // moment
@@ -23,13 +27,26 @@ const [FluxDispatch, Parse, TextInput, Popout, MessageActions, Clickable, Slider
         filter: (x) => x?.forEachRight, searchExports: false // lodash
     },
     {filter: x => x?.BACKGROUND_PRIMARY, searchExports: false}, // css vars var(--interactive-normal)
-    {filter: Webpack.Filters.byStrings('.USER(')}
+    {filter: Webpack.Filters.byStrings('.USER('), searchExports: true}, // fetch user
+    {filter: (x) => x?.intl}, // STRINGSSSS
+    {filter: x => x?.connectStores}, // flux
+    {filter: x => x?.ADD_REACTIONS, searchExports: true}, // permissions
+    {filter: x => x._savedDispatches, searchExports: true}, // component dispatch
+    {filter: Webpack.Filters.byStrings('HORIZONTAL_REVERSE', 'imageData'), searchExports: true},
+    {filter: Webpack.Filters.byStrings('Qp04hI'), searchExports: true}
 );
 
-// this should already exist.
-class BdApiComponents {
-
-}
+const LayerManager = {
+    push: (comp: React.ComponentType, fallback: React.ComponentType) => {
+        FluxDispatch.dispatch({type: 'LAYER_PUSH', component: comp, fallback});
+    },
+    pop: () => {
+        FluxDispatch.dispatch({type: "LAYER_POP"});
+    },
+    clear: () => {
+        FluxDispatch.dispatch({type: "LAYER_POP_ALL"});
+    }
+};
 
 // react components
 const Components = {
@@ -38,12 +55,15 @@ const Components = {
     Clickable,
     Slider,
     Modal,
-    Tooltip
+    Tooltip,
+    FormNotice,
+    ColorPicker
 };
 
 // flux and stuff
 const Common = {
     FluxDispatch,
+    Flux,
     moment,
     CloudUpload,
     hljs,
@@ -52,7 +72,12 @@ const Common = {
     MessageActions,
     cssVars,
     lodash,
+    fetchUser,
     Snowflake,
+    permissions,
+    LayerManager,
+    ComponentDispatch,
+    intl: {intl: Intl.intl, t: Intl.t},
     Modals: Webpack.getMangled(".modalKey?", {
         openModalLazy: Webpack.Filters.byStrings(".modalKey?"),
         openModal: Webpack.Filters.byStrings(",instant:"),
