6a708593d83f082a95dec9fe1684aebc0e04dd72
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-08-28 23:59:04 -0500

    Lint Fix

--- src/betterdiscord/modules/common.ts
+++ src/betterdiscord/modules/common.ts
@@ -28,39 +28,39 @@ const [
 ] = Webpack.getBulk(
     {filter: x => x._dispatch}, // flux dispatch
     {filter: x => x.parseTopic}, // parser
-    {filter: Webpack.Filters.byStrings('bmQU//', '+DFxLS'), searchExports: true}, // text input
+    {filter: Webpack.Filters.byStrings("bmQU//", "+DFxLS"), searchExports: true}, // text input
     {filter: Webpack.Filters.byStrings("Unsupported animation config: "), searchExports: true}, // popout
     {filter: Webpack.Filters.byKeys(["sendMessage", "editMessage"])}, // message actions
-    {filter: Webpack.Filters.byStrings('renderNonInteractive()'), searchExports: true}, // clickable
+    {filter: Webpack.Filters.byStrings("renderNonInteractive()"), searchExports: true}, // clickable
     {filter: Webpack.Filters.byStrings("markerPositions"), searchExports: true}, // slider
-    {filter: Webpack.Filters.byStrings('["size","title"'), searchExports: true}, // modal
+    {filter: Webpack.Filters.byStrings("[\"size\",\"title\""), searchExports: true}, // modal
     {filter: Webpack.Filters.byStrings("uploadFileToCloud"), searchExports: true}, // cloud uploader
-    {filter: Webpack.Filters.byStrings('useStateFromStores'), searchExports: true}, // state hook
+    {filter: Webpack.Filters.byStrings("useStateFromStores"), searchExports: true}, // state hook
     {filter: x => x?.parseTwoDigitYear}, // moment
     {filter: x => x?.registerLanguage}, // highlight.js
     {filter: x => x.extractTimestamp}, // snowflake
     {
-        filter: x => x.toString?.().includes('disabledText') &&
-            x.toString?.().includes('tooltipNote'),
+        filter: x => x.toString?.().includes("disabledText")
+            && x.toString?.().includes("tooltipNote"),
         searchExports: true
     }, // tooltip
     {
         filter: (x) => x?.forEachRight, searchExports: false // lodash
     },
     {filter: x => x?.BACKGROUND_PRIMARY, searchExports: false}, // css variables
-    {filter: Webpack.Filters.byStrings('.USER('), searchExports: true}, // fetch user
+    {filter: Webpack.Filters.byStrings(".USER("), searchExports: true}, // fetch user
     {filter: (x) => x?.intl}, // internationalization
     {filter: x => x?.connectStores}, // flux
     {filter: x => x?.ADD_REACTIONS, searchExports: true}, // permissions
     {filter: x => x._savedDispatches, searchExports: true}, // component dispatch
-    {filter: Webpack.Filters.byStrings('HORIZONTAL_REVERSE', 'imageData'), searchExports: true}, // form notice
-    {filter: Webpack.Filters.byStrings('Qp04hI'), searchExports: true}, // color picker
+    {filter: Webpack.Filters.byStrings("HORIZONTAL_REVERSE", "imageData"), searchExports: true}, // form notice
+    {filter: Webpack.Filters.byStrings("Qp04hI"), searchExports: true}, // color picker
     {filter: m => m.getChannelIconURL} // image utils
 );
 
 const layerManager = {
     push: (comp: React.ComponentType, fallback: React.ComponentType) => {
-        fluxDispatch.dispatch({type: 'LAYER_PUSH', component: comp, fallback});
+        fluxDispatch.dispatch({type: "LAYER_PUSH", component: comp, fallback});
     },
     pop: () => {
         fluxDispatch.dispatch({type: "LAYER_POP"});
--- src/common/utils/forceUpdate.ts
+++ src/common/utils/forceUpdate.ts
@@ -1,4 +1,4 @@
-import ReactUtils from '@api/reactutils';
+import ReactUtils from "@api/reactutils";
 import Patcher from "@modules/patcher";
 
 /* Skamt Code */
@@ -6,6 +6,6 @@ export function reRender(selector: string) {
     const target = document.querySelector(selector)?.parentElement;
     if (!target) return;
     const instance = ReactUtils.getOwnerInstance(target);
-    const unpatch = Patcher.instead('BetterDiscord-ReRender', instance, "render", () => unpatch());
+    const unpatch = Patcher.instead("BetterDiscord-ReRender", instance, "render", () => unpatch());
     instance.forceUpdate(() => instance.forceUpdate());
 }
