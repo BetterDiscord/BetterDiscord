4e5ab92fc592eccbb2ffcaf8dab5d1fead7b8242
KingGamingYT <45918062+KingGamingYT@users.noreply.github.com>
2025-12-10 18:08:47 -0700

    section header + changelog button

--- src/betterdiscord/ui/settings.tsx
+++ src/betterdiscord/ui/settings.tsx
@@ -75,7 +75,7 @@ interface SidebarItemLayout {
 }
 
 interface SectionLayout {
-    useLabel(): React.ReactNode;
+    useTitle(): React.ReactNode;
     buildLayout(): SidebarItemLayout[];
     usePredicate?(): boolean;
 }
@@ -404,7 +404,7 @@ export default new class SettingsRenderer {
 
                 return layouts;
             },
-            useLabel: () => Object.assign(<LayerSettingTitle />, {toString: () => "BetterDiscord"}),
+            useTitle: () => Object.assign(<LayerSettingTitle />, {toString: () => "BetterDiscord"}),
         });
 
         Patcher.after("SettingsManager", rootLayout, "buildLayout", (that, args, res) => {
