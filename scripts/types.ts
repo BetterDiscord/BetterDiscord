02efae624f8d0983de1d40d29051de223da1ed3c
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-04-16 22:57:29 -0400

    Make generated types not suck

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,6 +1,8 @@
+import tsconfig from "../tsconfig.json";
 import {$} from "bun";
 import {rollup} from "rollup";
 import {dts} from "rollup-plugin-dts";
+import {readFile, writeFile} from "node:fs/promises";
 
 buildTypes();
 
@@ -14,32 +16,36 @@ async function buildTypes() {
         plugins: [dts({
             compilerOptions: {
                 baseUrl: "./dist/declarations",
-                paths: {
-                    "@assets/*": ["./assets/*"],
-                    "@common/*": ["./src/common/*"],
-                    "@builtins/*": ["./src/betterdiscord/builtins/*"],
-                    "@data/*": ["./src/betterdiscord/data/*"],
-                    "@api/*": ["./src/betterdiscord/api/*"],
-                    "@modules/*": ["./src/betterdiscord/modules/*"],
-                    "@polyfill/*": ["./src/betterdiscord/polyfill/*"],
-                    "@stores/*": ["./src/betterdiscord/stores/*"],
-                    "@structs/*": ["./src/betterdiscord/structs/*"],
-                    "@styles/*": ["./src/betterdiscord/styles/*"],
-                    "@ui/*": ["./src/betterdiscord/ui/*"],
-                    "@utils/*": ["./src/betterdiscord/utils/*"],
-                    "@typed/*": ["./src/betterdiscord/types/*"],
-                    "@webpack": ["./src/betterdiscord/webpack"]
-                }
+                paths: tsconfig.compilerOptions.paths
             },
             includeExternal: ["clsx"]
         })]
     });
 
     await bundle.write({
-        file: "dist/bdapi.d.ts",
+        file: "./dist/bdapi.d.ts",
         format: "es"
     });
 
     await bundle.close();
+
+    console.log("Finalizing bdapi.d.ts...");
+    let content = await readFile("./dist/bdapi.d.ts", "utf-8");
+
+    // Remove declarations and put everything in a namespace
+    content = content.replaceAll("\r\n", "\n")
+        .replace("\ndeclare const React$1: typeof react;", "")
+        .replaceAll("React$1", "react")
+        .replaceAll("\ndeclare ", "\n")
+        .replace(/\nexport .+\n/, "");
+
+    const insertAt = content.indexOf("\n", content.lastIndexOf("import")) + 2;
+    const namespaces = "declare global {\nnamespace BetterDiscord {\n";
+    const declaration = "\n\nconst BdApi: BetterDiscord.BdApi;\ninterface Window {\n    BdApi: BetterDiscord.BdApi;\n}";
+    const header = "export {};\n\n";
+
+    content = header + content.slice(0, insertAt) + namespaces + content.slice(insertAt + 1) + "\n}" + declaration + "\n}";
+
+    await writeFile("./dist/bdapi.d.ts", content, "utf-8");
     console.log("Done!");
 }
