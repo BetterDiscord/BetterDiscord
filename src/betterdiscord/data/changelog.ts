17d21747b2022539ae033662e3f3ed898fbcb0e9
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2026-08-24 20:21:07 -0700

    Add video

--- src/betterdiscord/data/changelog.ts
+++ src/betterdiscord/data/changelog.ts
@@ -6,7 +6,7 @@ export default {
     title: "BetterDiscord",
     subtitle: `v${config.get("version")}`,
     // https://youtu.be/BZq1eb9d0HI?si=67V2eArlF4atnGnz
-    // video: "https://www.youtube.com/embed/BZq1eb9d0HI?si=67V2eArlF4atnGnz&vq=hd720p&hd=1&rel=0&showinfo=0&mute=0&loop=1&autohide=1",
+    video: "https://www.youtube.com/embed/Qv1HUqqUgkg??si=67V2eArlF4atnGnz&vq=hd720p&hd=1&rel=0&showinfo=0&mute=0&loop=1&autohide=1",
     // banner: "https://i.imgur.com/wuh5yMK.png",
     blurb: "New Injection Style and Bug fixes!",
     changes: [
